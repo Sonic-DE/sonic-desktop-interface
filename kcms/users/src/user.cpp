b339b45ed8f0dea31281b9dc61234912d8d14b9f
Adriaan de Groot <groot@kde.org>
2020-06-07 20:17:58 +0200

    Handle absent <crypt.h>
    The prototype for crypt(3) is in <crypt.h> on Linux (apparently),
    but in <unistd.h> on FreeBSD. There's already machinery for
    detecting the presence of that file in CMake, so use that.
    
    Reported and resolved independently in
    	https://phabricator.kde.org/D29847

--- kcms/users/src/user.cpp
+++ kcms/users/src/user.cpp
@@ -22,7 +22,12 @@
 #include "user.h"
 #include "user_interface.h"
 
+#include "config-unix.h"
+
+#ifdef HAVE_CRYPT_H
 #include <crypt.h>
+#endif
+
 #include <unistd.h>
 #include <sys/types.h>
 #include <QtConcurrent>
