b9ab3b39c43847c8e85aed8de0868b4d53c64276
Marco Martin <notmart@gmail.com>
2021-01-28 17:08:05 +0100

    migrate from MidButton to MiddleButton
    the MidButton name first got deprecated, then doesn't really work anymore
    this fixes middle button action on containments
    BUG:430418

---
+++ desktoppackage/contents/updates/containmentactions_middlebutton.js
@@ -0,0 +1,13 @@
+var plasmaConfig = ConfigFile("plasma-org.kde.plasma.desktop-appletsrc", "ActionPlugins");
+
+for (let i in plasmaConfig.groupList) {
+    let subGroup = ConfigFile(plasmaConfig, plasmaConfig.groupList[i])
+
+    for (let j in subGroup.keyList) {
+        let key = subGroup.keyList[j];
+        if (key.indexOf("MidButton") !== -1) {
+            subGroup.writeEntry(key.replace("MidButton", "MiddleButton"), subGroup.readEntry(key));
+            subGroup.deleteEntry(key);
+        }
+    }
+}
