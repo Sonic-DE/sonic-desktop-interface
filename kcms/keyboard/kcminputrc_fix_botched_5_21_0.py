95398571e43e5da490b4a9dd3e1580e8a90e3a39
Jan Blackquill <uhhadd@gmail.com>
2021-02-18 01:17:14 -0500

    kcms/keyboard: handle botched 5.21.0 migration
    Since we shipped a botched migration script for 5.21.0, we need to
    ship a new migration script that un-botches the effects of the 5.21.0
    script. We also change the ID of the plain kcminput_repeat migration so
    that we can distinguish between the botched migration and the non-botched
    migration, and only reverse migrations applied by the botched migration.
    
    BUG: 431923
    FIXED-IN: 5.21.1

---
+++ kcms/keyboard/kcminputrc_fix_botched_5_21_0.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import sys
+
+#
+#    SPDX-FileCopyrightText: 2021 Janet Blackquill <uhhadd@gmail.com>
+#
+#    SPDX-License-Identifier: LGPL-2.0-or-later
+#
+
+content = sys.stdin.read()
+
+# 5.21.0 botched the migration by accidentally turning enabled to disabled and disabled to enabled
+# so this script checks if the botched migration was applied, and if so, reverses it.
+if "kcminputrc_repeat.upd:kcminputrc_migrate_repeat_value" in content:
+    print("# DELETE KeyRepeat")
+
+    if "KeyRepeat=repeat" in content:
+        print("KeyRepeat=nothing")
+    elif "KeyRepeat=nothing" in content:
+        print("KeyRepeat=repeat")
