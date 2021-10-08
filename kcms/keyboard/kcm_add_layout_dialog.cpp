9326460819017d0693ce85810789723b713a2a9c
Bharadwaj Raju <bharadwaj.raju777@protonmail.com>
2021-10-08 22:22:54 +0530

    [kcms/keyboard] Redesign "Add Layout" dialog
    Instead of having a three-part language+layout+variant design, it now has a single list of layouts + search

--- kcms/keyboard/kcm_add_layout_dialog.cpp
+++ kcms/keyboard/kcm_add_layout_dialog.cpp
@@ -21,29 +21,33 @@ AddLayoutDialog::AddLayoutDialog(const Rules *rules_, Flags *flags_, const QStri
     , flags(flags_)
     , model(model_)
     , options(options_)
-    , selectedLanguage(QStringLiteral("no_language"))
 {
     layoutDialogUi = new Ui_AddLayoutDialog();
     layoutDialogUi->setupUi(this);
 
-    QSet<QString> languages;
-    for (const LayoutInfo *layoutInfo : rules->layoutInfos) {
-        QSet<QString> langs = QSet<QString>(layoutInfo->languages.constBegin(), layoutInfo->languages.constEnd());
-        languages.unite(langs);
+    for (const LayoutInfo *layoutInfo : qAsConst(rules->layoutInfos)) {
+        if (flags) {
+            QIcon icon(flags->getIcon(layoutInfo->name));
+            if (icon.isNull()) {
+                QPixmap emptyPixmap(layoutDialogUi->layoutListWidget->iconSize());
+                emptyPixmap.fill(Qt::transparent);
+                icon = QIcon(emptyPixmap); // align text with no icons
+            }
+            for (const VariantInfo *variantInfo : layoutInfo->variantInfos) {
+                QListWidgetItem *newItem = new QListWidgetItem(icon, variantInfo->description);
+                newItem->setData(LayoutNameRole, layoutInfo->name);
+                newItem->setData(VariantNameRole, variantInfo->name);
+                layoutDialogUi->layoutListWidget->addItem(newItem);
+            }
+        } else {
+            for (const VariantInfo *variantInfo : layoutInfo->variantInfos) {
+                QListWidgetItem *newItem = new QListWidgetItem(variantInfo->description);
+                newItem->setData(LayoutNameRole, layoutInfo->name);
+                newItem->setData(VariantNameRole, variantInfo->name);
+                layoutDialogUi->layoutListWidget->addItem(newItem);
+            }
+        }
     }
-    IsoCodes isoCodes(IsoCodes::iso_639_3);
-    for (const QString &lang : qAsConst(languages)) {
-        const IsoCodeEntry *isoCodeEntry = isoCodes.getEntry(IsoCodes::attr_iso_639_3_id, lang);
-        //    	const IsoCodeEntry* isoCodeEntry = isoCodes.getEntry(IsoCodes::attr_iso_639_2B_code, lang);
-        //    	if( isoCodeEntry == NULL ) {
-        //    		isoCodeEntry = isoCodes.getEntry(IsoCodes::attr_iso_639_2T_code, lang);
-        //    	}
-        QString name = isoCodeEntry != nullptr ? i18n(isoCodeEntry->value(IsoCodes::attr_name).toUtf8()) : lang;
-        layoutDialogUi->languageComboBox->addItem(name, lang);
-    }
-    layoutDialogUi->languageComboBox->model()->sort(0);
-    layoutDialogUi->languageComboBox->insertItem(0, i18n("Any language"), "");
-    layoutDialogUi->languageComboBox->setCurrentIndex(0);
 
     if (showLabel) {
         layoutDialogUi->labelEdit->setMaxLength(LayoutUnit::MAX_LABEL_LENGTH);
@@ -52,90 +56,46 @@ AddLayoutDialog::AddLayoutDialog(const Rules *rules_, Flags *flags_, const QStri
         layoutDialogUi->labelEdit->setVisible(false);
     }
 
-    languageChanged(0);
-    connect(layoutDialogUi->languageComboBox, static_cast<void (QComboBox::*)(int)>(&QComboBox::activated), this, &AddLayoutDialog::languageChanged);
-    connect(layoutDialogUi->layoutComboBox, static_cast<void (QComboBox::*)(int)>(&QComboBox::activated), this, &AddLayoutDialog::layoutChanged);
+    connect(layoutDialogUi->layoutSearchField,
+            static_cast<void (QLineEdit::*)(const QString &)>(&QLineEdit::textChanged),
+            this,
+            &AddLayoutDialog::layoutSearched);
+    connect(layoutDialogUi->layoutListWidget,
+            static_cast<void (QListWidget::*)(QListWidgetItem *, QListWidgetItem *)>(&QListWidget::currentItemChanged),
+            this,
+            &AddLayoutDialog::layoutChanged);
 
     connect(layoutDialogUi->prevbutton, &QPushButton::clicked, this, &AddLayoutDialog::preview);
     layoutDialogUi->prevbutton->setVisible(Tastenbrett::exists());
 }
 
-void AddLayoutDialog::languageChanged(int langIdx)
+void AddLayoutDialog::layoutSearched(const QString &text)
 {
-    QString lang = layoutDialogUi->languageComboBox->itemData(langIdx).toString();
-    if (lang == selectedLanguage)
-        return;
-
-    QPixmap emptyPixmap(layoutDialogUi->layoutComboBox->iconSize());
-    emptyPixmap.fill(Qt::transparent);
-
-    layoutDialogUi->layoutComboBox->clear();
-    int defaultIndex = -1;
-    int i = 0;
-    for (const LayoutInfo *layoutInfo : qAsConst(rules->layoutInfos)) {
-        if (lang.isEmpty() || layoutInfo->isLanguageSupportedByLayout(lang)) {
-            if (flags) {
-                QIcon icon(flags->getIcon(layoutInfo->name));
-                if (icon.isNull()) {
-                    icon = QIcon(emptyPixmap); // align text with no icons
-                }
-                layoutDialogUi->layoutComboBox->addItem(icon, layoutInfo->description, layoutInfo->name);
-            } else {
-                layoutDialogUi->layoutComboBox->addItem(layoutInfo->description, layoutInfo->name);
+    QListWidget *list = layoutDialogUi->layoutListWidget;
+    for (int row = 0; row < list->count(); ++row) {
+        bool match = (list->item(row)->text().contains(text, Qt::CaseInsensitive));
+        QStringList words = text.split(QLatin1Char(' '), Qt::SkipEmptyParts);
+        if (!match) {
+            match = true;
+            for (const QString &word : words) {
+                match = match && list->item(row)->text().contains(word, Qt::CaseInsensitive);
             }
-
-            // try to guess best default layout selection for given language
-            if (!lang.isEmpty() && defaultIndex == -1 && layoutInfo->isLanguageSupportedByDefaultVariant(lang)) {
-                defaultIndex = i;
-            }
-            i++;
         }
+        list->item(row)->setHidden(!match);
     }
-    if (defaultIndex == -1) {
-        defaultIndex = 0;
-    }
-
-    layoutDialogUi->layoutComboBox->model()->sort(0);
-    layoutDialogUi->layoutComboBox->setCurrentIndex(defaultIndex);
-    layoutChanged(defaultIndex);
-
-    selectedLanguage = lang;
 }
 
-void AddLayoutDialog::layoutChanged(int layoutIdx)
+void AddLayoutDialog::layoutChanged(QListWidgetItem *current, QListWidgetItem *previous)
 {
-    QString layoutName = layoutDialogUi->layoutComboBox->itemData(layoutIdx).toString();
-    if (layoutName == selectedLayout)
-        return;
-
-    QString lang = layoutDialogUi->languageComboBox->itemData(layoutDialogUi->languageComboBox->currentIndex()).toString();
-
-    layoutDialogUi->variantComboBox->clear();
-    const LayoutInfo *layoutInfo = rules->getLayoutInfo(layoutName);
-    for (const VariantInfo *variantInfo : layoutInfo->variantInfos) {
-        if (lang.isEmpty() || layoutInfo->isLanguageSupportedByVariant(variantInfo, lang)) {
-            layoutDialogUi->variantComboBox->addItem(variantInfo->description, variantInfo->name);
-        }
-    }
-
-    layoutDialogUi->variantComboBox->model()->sort(0);
-
-    if (lang.isEmpty() || layoutInfo->isLanguageSupportedByDefaultVariant(lang)) {
-        layoutDialogUi->variantComboBox->insertItem(0, i18nc("variant", "Default"), "");
-    }
-    layoutDialogUi->variantComboBox->setCurrentIndex(0);
-
-    layoutDialogUi->labelEdit->setText(layoutName);
-
-    selectedLayout = layoutName;
+    selectedLayout = current->data(LayoutNameRole).toString();
+    selectedLayoutUnit.setLayout(selectedLayout);
+    selectedLayoutUnit.setVariant(current->data(VariantNameRole).toString());
 }
 
 void AddLayoutDialog::accept()
 {
-    selectedLayoutUnit.setLayout(layoutDialogUi->layoutComboBox->itemData(layoutDialogUi->layoutComboBox->currentIndex()).toString());
-    selectedLayoutUnit.setVariant(layoutDialogUi->variantComboBox->itemData(layoutDialogUi->variantComboBox->currentIndex()).toString());
     QString label = layoutDialogUi->labelEdit->text();
-    if (label == selectedLayoutUnit.layout()) {
+    if (label == selectedLayout) {
         label = QLatin1String("");
     }
     selectedLayoutUnit.setDisplayName(label);
@@ -145,7 +105,6 @@ void AddLayoutDialog::accept()
 
 void AddLayoutDialog::preview()
 {
-    int index = layoutDialogUi->variantComboBox->currentIndex();
-    QString variant = layoutDialogUi->variantComboBox->itemData(index).toString();
+    QString variant = layoutDialogUi->layoutListWidget->currentItem()->data(VariantNameRole).toString();
     Tastenbrett::launch(model, selectedLayout, variant, options.join(','));
 }
