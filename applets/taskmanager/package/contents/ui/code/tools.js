71d684fce17a9b52b5a9f575ab0b3e7436d5e42f
Bharadwaj Raju <bharadwaj.raju777@protonmail.com>
2021-10-22 00:53:34 +0530

    Don't open other tooltips by hover if current tooltip was opened by click

--- applets/taskmanager/package/contents/ui/code/tools.js
+++ applets/taskmanager/package/contents/ui/code/tools.js
@@ -148,6 +148,7 @@ function activateTask(index, model, modifiers, task) {
         else if (plasmoid.configuration.showToolTips
             && plasmoid.configuration.groupedTaskVisualization === 1
         ) {
+            tasks.toolTipOpenedByClick = task;
             task.showToolTip();
         }
 
