c9aba874c874575bd565a7eaf650b5b1d278faab
Jan Blackquill <uhhadd@gmail.com>
2022-06-22 22:21:23 -0400

    folderview: make menu entries less ambiguous
    Without clear expectations, 'Columns' and 'Rows' are kinda meaningless to
    users, since users from different backgrounds will form different expectations
    as to what their behaviour will be, e.g. users that have used design software may
    be used to selecting five items and selecting 'Layout > Columns', which would turn
    that into five columns of one item each, while users that use different software
    (or GUI layouts) would expect that would create one column of five items.
    
    In short: users have contradictory expectations if we use 'Columns' and 'Rows' as the
    UI strings here, so let's use something less contradictory.
    
    BUG: 455770

--- containments/desktop/plugins/folder/viewpropertiesmenu.cpp
+++ containments/desktop/plugins/folder/viewpropertiesmenu.cpp
@@ -63,14 +63,16 @@ ViewPropertiesMenu::ViewPropertiesMenu(QObject *parent)
         m_iconSize->addAction(action);
     }
 
-    m_arrangementMenu = m_menu->addMenu(QIcon::fromTheme(QStringLiteral("object-rows")), i18n("Arrange In"));
+    m_arrangementMenu = m_menu->addMenu(QIcon::fromTheme(QStringLiteral("object-rows")), i18nc("@item:inmenu arrangement of icons", "Arrange"));
     m_arrangement = new QActionGroup(this);
     connect(m_arrangement, &QActionGroup::triggered, this, &ViewPropertiesMenu::arrangementChanged);
-    action = m_arrangementMenu->addAction(i18nc("@item:inmenu arrangement of icons", "Rows"));
+    const auto isLtR = QGuiApplication::layoutDirection() == Qt::LeftToRight;
+    action = m_arrangementMenu->addAction(isLtR ? i18nc("@item:inmenu arrangement of icons", "Left To Right")
+                                                : i18nc("@item:inmenu arrangement of icons", "Right to Left"));
     action->setCheckable(true);
     action->setData(0);
     m_arrangement->addAction(action);
-    action = m_arrangementMenu->addAction(i18nc("@item:inmenu arrangement of icons", "Columns"));
+    action = m_arrangementMenu->addAction(i18nc("@item:inmenu arrangement of icons", "Top To Bottom"));
     action->setData(1);
     action->setCheckable(true);
     m_arrangement->addAction(action);
