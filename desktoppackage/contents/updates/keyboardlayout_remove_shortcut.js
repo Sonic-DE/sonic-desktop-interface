67b47816b193dc9747136a778e0efbd9557486ea
Andrey Butirsky <butirsky@gmail.com>
2022-07-28 12:57:15 +0300

    remove global shortcut for keyboardlayout applet

---
+++ desktoppackage/contents/updates/keyboardlayout_remove_shortcut.js
@@ -0,0 +1,51 @@
+function forEachWidgetInContainmentList(containmentList, callback) {
+    for (var containmentIndex = 0; containmentIndex < containmentList.length; containmentIndex++) {
+        var containment = containmentList[containmentIndex];
+
+        var widgets = containment.widgets();
+        for (var widgetIndex = 0; widgetIndex < widgets.length; widgetIndex++) {
+            var widget = widgets[widgetIndex];
+            callback(widget, containment);
+        }
+    }
+}
+
+function forEachWidget(callback) {
+    forEachWidgetInContainmentList(desktops(), callback);
+    forEachWidgetInContainmentList(panels(), callback);
+}
+
+function forEachWidgetByType(type, callback) {
+    forEachWidget(function(widget, containment) {
+        if (widget.type == type) {
+            callback(widget, containment);
+        }
+    });
+}
+
+function logWidget(widget) {
+    print("" + widget.type + ": ");
+
+    var configGroups = widget.configGroups.slice(); // slice is used to clone the array
+    for (var groupIndex = 0; groupIndex < configGroups.length; groupIndex++) {
+        var configGroup = configGroups[groupIndex];
+        print("\t" + configGroup + ": ");
+        widget.currentConfigGroup = [configGroup];
+
+        for (var keyIndex = 0; keyIndex < widget.configKeys.length; keyIndex++) {
+            var configKey = widget.configKeys[keyIndex];
+            var configValue = widget.readConfig(configKey);
+            print("\t\t" + configKey + ": " + configValue);
+        }
+    }
+}
+
+//--- Log all widgets
+// forEachWidget(function(widget){
+//     logWidget(widget);
+// });
+
+//--- Log only digitalclock widgets
+forEachWidgetByType("org.kde.plasma.digitalclock", function(widget){
+    logWidget(widget);
+});
