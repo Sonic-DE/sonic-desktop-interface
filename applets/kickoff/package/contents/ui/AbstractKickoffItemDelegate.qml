KDE/plasma-desktop

commit 0868a5ae8154d10ccc7488034bc5b6e458a17968

applets/kickoff: add "compact list item style" option and use new tall style by default
Kickoff uses an unusual way of locating list items' descriptions: it
puts them on the same line as the title, aligned to the right.

This saves vertical space but suffers from several issues:

- Because both labels need to inhabit the same line, a long title will
  cause the description to be elided into uselessness
- It's a different style than what we use for other list items with
  fundamentally the same set of data; we don't re-use users' existing
  familiarity with KDE-style list items that typically have the caption,
  subtitle, or description below the label/title
- Saving vertical space is not a win here because we want the applet to
  be touch-friendly, and short list items make that worse, not better

Accordingly, this MR adds an option to switch the list items between two
styles:
- The current style, which is now called "compact list item style"
- A new default style with the subtitle below the main text, which is more
  conventional and more touch friendly.

This new default list item style is intended to improve the "Gwenview? What's
a Gwenview?" effect that users have been complaining about. Despite the fact
that the compact/inline list items have a caption that actually tells you what
branded apps like Gwenview are, people seem blind to it because they don't
expect to see a description in that location. No other KDE list items do this
and it's a fairly unusual style outside of KDE as well.

When using Touch mode, compact mode is disabled if it was manually activated.


| New default setting (tall list items) | compact list items | Config window now |
| ------------------------------------- | ------------------ | ----------------- |
| ![Tall_style](/uploads/9af537fbbabe461aff7a170139b8095f/Tall_style.png) | ![compact_mode](/uploads/ce119b5fb345f2b784bbbba9e73a2889/compact_mode.png) | ![Config_window](/uploads/2204db79fbdf06f653d1ae968a472424/Config_window.png) |

applets/kickoff/package/contents/ui/AbstractKickoffItemDelegate.qml
@@ -5,6 +5,7 @@
     Copyright (C) 2015-2018  Eike Hein <hein@kde.org>
     Copyright (C) 2021 by Mikel Johnson <mikel5764@gmail.com>
     Copyright (C) 2021 by Noah Davis <noahadvs@gmail.com>
+    Copyright (C) 2022 Nate Graham <nate@kde.org>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -40,7 +41,6 @@ T.ItemDelegate {
     required property string description
 
     readonly property Flickable view: ListView.view ?? GridView.view
-    readonly property bool textUnderIcon: display === PC3.AbstractButton.TextUnderIcon
     property bool isCategory: false
     readonly property bool hasActionList: model && (model.favoriteId !== null || ("hasActionList" in model && model.hasActionList === true))
     property var actionList: null
@@ -48,6 +48,10 @@ T.ItemDelegate {
 
     property bool dragEnabled: enabled && !isCategory
         && plasmoid.immutability !== PlasmaCore.Types.SystemImmutable
+
+    property bool labelTruncated: false
+    property bool descriptionTruncated: false
+    property bool descriptionVisible: true
 
     function openActionMenu(x = undefined, y = undefined) {
         if (!hasActionList) { return; }
@@ -88,21 +92,10 @@ T.ItemDelegate {
     implicitHeight: Math.max(implicitBackgroundHeight + topInset + bottomInset,
                              implicitContentHeight + topPadding + bottomPadding)
 
-    // We use an increased fixed vertical padding to improve touch usability
-    leftPadding: KickoffSingleton.listItemMetrics.margins.left
-        + (!textUnderIcon && mirrored ? KickoffSingleton.fontMetrics.descent : 0)
-    rightPadding: KickoffSingleton.listItemMetrics.margins.right
-        + (!textUnderIcon && !mirrored ? KickoffSingleton.fontMetrics.descent : 0)
-    topPadding: PlasmaCore.Units.smallSpacing*2
-    bottomPadding: PlasmaCore.Units.smallSpacing*2
-
     spacing: KickoffSingleton.fontMetrics.descent
 
     enabled: !model.disabled
     hoverEnabled: false
-
-    icon.width: PlasmaCore.Units.iconSizes.smallMedium
-    icon.height: PlasmaCore.Units.iconSizes.smallMedium
 
     text: model.name ?? model.display
     Accessible.description: root.description != root.text ? root.description : ""
@@ -126,55 +119,6 @@ T.ItemDelegate {
                 }
             }
         }
-    }
-
-    background: null
-    contentItem: GridLayout {
-        baselineOffset: label.y + label.baselineOffset
-        columnSpacing: parent.spacing
-        rowSpacing: parent.spacing
-        flow: root.textUnderIcon ? GridLayout.TopToBottom : GridLayout.LeftToRight
-        PlasmaCore.IconItem {
-            id: iconItem
-            Layout.alignment: root.textUnderIcon ? Qt.AlignHCenter | Qt.AlignBottom : Qt.AlignLeft | Qt.AlignVCenter
-            implicitWidth: root.icon.width
-            implicitHeight: root.icon.height
-            animated: false
-            usesPlasmaTheme: false
-            source: root.decoration || root.icon.name || root.icon.source
-        }
-        PC3.Label {
-            id: label
-            Layout.alignment: root.textUnderIcon ? Qt.AlignHCenter | Qt.AlignTop : Qt.AlignLeft | Qt.AlignVCenter
-            Layout.fillWidth: true
-            Layout.preferredHeight: root.textUnderIcon && lineCount === 1 ? implicitHeight * 2 : implicitHeight
-            text: root.text
-            elide: Text.ElideRight
-            horizontalAlignment: root.textUnderIcon ? Text.AlignHCenter : Text.AlignLeft
-            verticalAlignment: root.textUnderIcon ? Text.AlignTop : Text.AlignVCenter
-            maximumLineCount: 2
-            wrapMode: Text.Wrap
-            textFormat: root.model && root.model.isMultilineText ? Text.StyledText : Text.PlainText
-        }
-    }
-
-    PC3.Label {
-        id: descriptionLabel
-        parent: root
-        anchors {
-            left: root.contentItem.left
-            right: root.contentItem.right
-            baseline: root.contentItem.baseline
-            leftMargin: root.textUnderIcon ? 0 : root.implicitContentWidth + root.spacing
-            baselineOffset: root.textUnderIcon ? implicitHeight : 0
-        }
-        visible: !textUnderIcon && text.length > 0
-        enabled: false
-        text: root.Accessible.description
-        elide: Text.ElideRight
-        horizontalAlignment: root.textUnderIcon ? Text.AlignHCenter : Text.AlignRight
-        verticalAlignment: root.textUnderIcon ? Text.AlignTop : Text.AlignVCenter
-        maximumLineCount: 1
     }
 
     Drag.active: mouseArea.drag.active
@@ -250,15 +194,15 @@ T.ItemDelegate {
     }
 
     PC3.ToolTip.text: {
-        const showDescription = descriptionLabel.truncated
-            || (textUnderIcon && Accessible.description.length > 0)
-        if (label.truncated && showDescription) {
+        if (root.labelTruncated && root.descriptionTruncated) {
             return `${text} (${description})`
-        } else if (showDescription) {
+        } else if (root.descriptionTruncated || !root.descriptionVisible) {
             return description
         }
         return ""
     }
     PC3.ToolTip.visible: mouseArea.containsMouse && PC3.ToolTip.text.length > 0
     PC3.ToolTip.delay: Kirigami.Units.toolTipDelay
+
+    background: null
 }

applets/kickoff/package/contents/ui/KickoffGridDelegate.qml
@@ -1,0 +1,72 @@
+/*
+    Copyright (C) 2011  Martin *Gräßlin <mgraesslin@kde.org>
+    Copyright (C) 2012  Gregor Taetzner <gregor@freenet.de>
+    Copyright 2014 Sebastian Kügler <sebas@kde.org>
+    Copyright (C) 2015-2018  Eike Hein <hein@kde.org>
+    Copyright (C) 2021 by Mikel Johnson <mikel5764@gmail.com>
+    Copyright (C) 2021 by Noah Davis <noahadvs@gmail.com>
+    Copyright (C) 2022 Nate Graham <nate@kde.org>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+import QtQuick 2.15
+import QtQml 2.15
+import QtQuick.Layouts 1.15
+import org.kde.plasma.core 2.0 as PlasmaCore
+import org.kde.plasma.components 3.0 as PC3
+
+AbstractKickoffItemDelegate {
+    id: root
+
+    leftPadding: KickoffSingleton.listItemMetrics.margins.left
+    rightPadding: KickoffSingleton.listItemMetrics.margins.right
+    topPadding: PlasmaCore.Units.smallSpacing * 2
+    bottomPadding: PlasmaCore.Units.smallSpacing * 2
+
+    icon.width: PlasmaCore.Units.iconSizes.large
+    icon.height: PlasmaCore.Units.iconSizes.large
+
+    labelTruncated: label.truncated
+    descriptionVisible: false
+
+    contentItem: ColumnLayout {
+        spacing: root.spacing
+
+        PlasmaCore.IconItem {
+            implicitWidth: root.icon.width
+            implicitHeight: root.icon.height
+            Layout.alignment: Qt.AlignHCenter | Qt.AlignBottom
+
+            animated: false
+            usesPlasmaTheme: false
+
+            source: root.decoration || root.icon.name || root.icon.source
+        }
+
+        PC3.Label {
+            id: label
+            Layout.alignment: Qt.AlignHCenter | Qt.AlignTop
+            Layout.fillWidth: true
+            Layout.preferredHeight: label.lineCount === 1 ? label.implicitHeight * 2 : label.implicitHeight
+
+            text: root.text
+            elide: Text.ElideRight
+            horizontalAlignment: Text.AlignHCenter
+            verticalAlignment: Text.AlignTop
+            maximumLineCount: 2
+            wrapMode: Text.Wrap
+        }
+    }
+}

applets/kickoff/package/contents/ui/KickoffListDelegate.qml
@@ -1,0 +1,109 @@
+/*
+    Copyright (C) 2011  Martin *Gräßlin <mgraesslin@kde.org>
+    Copyright (C) 2012  Gregor Taetzner <gregor@freenet.de>
+    Copyright 2014 Sebastian Kügler <sebas@kde.org>
+    Copyright (C) 2015-2018  Eike Hein <hein@kde.org>
+    Copyright (C) 2021 by Mikel Johnson <mikel5764@gmail.com>
+    Copyright (C) 2021 by Noah Davis <noahadvs@gmail.com>
+    Copyright (C) 2022 Nate Graham <nate@kde.org>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+import QtQuick 2.15
+import QtQml 2.15
+import QtQuick.Layouts 1.15
+import org.kde.plasma.core 2.0 as PlasmaCore
+import org.kde.plasma.components 3.0 as PC3
+import org.kde.kirigami 2.16 as Kirigami
+
+AbstractKickoffItemDelegate {
+    id: root
+
+    property bool compact: Kirigami.Settings.tabletMode ? false : plasmoid.configuration.compactMode
+
+    leftPadding: KickoffSingleton.listItemMetrics.margins.left
+    + (mirrored ? KickoffSingleton.fontMetrics.descent : 0)
+    rightPadding: KickoffSingleton.listItemMetrics.margins.right
+    + (!mirrored ? KickoffSingleton.fontMetrics.descent : 0)
+    // Otherwise it's *too* compact :)
+    topPadding: compact ? PlasmaCore.Units.mediumSpacing : PlasmaCore.Units.smallSpacing
+    bottomPadding: compact ? PlasmaCore.Units.mediumSpacing : PlasmaCore.Units.smallSpacing
+
+    icon.width: compact || root.isCategory ? PlasmaCore.Units.iconSizes.smallMedium : PlasmaCore.Units.iconSizes.medium
+    icon.height: compact || root.isCategory ? PlasmaCore.Units.iconSizes.smallMedium : PlasmaCore.Units.iconSizes.medium
+
+    labelTruncated: label.truncated
+    descriptionTruncated: descriptionLabel.truncated
+    descriptionVisible: descriptionLabel.visible
+
+    contentItem: RowLayout {
+        id: row
+        spacing: KickoffSingleton.listItemMetrics.margins.left * 2
+
+        PlasmaCore.IconItem {
+            id: icon
+            implicitWidth: root.icon.width
+            implicitHeight: root.icon.height
+            Layout.alignment: Qt.AlignLeft | Qt.AlignVCenter
+
+            animated: false
+            usesPlasmaTheme: false
+
+            source: root.decoration || root.icon.name || root.icon.source
+        }
+
+        GridLayout {
+            id: gridLayout
+            Layout.fillWidth: true
+
+            rows: root.compact ? 1 : 2
+            columns: root.compact ? 2 : 1
+            rowSpacing: 0
+            columnSpacing: root.spacing
+
+            PC3.Label {
+                id: label
+                Layout.fillWidth: !descriptionLabel.visible
+                Layout.maximumWidth: root.width - root.leftPadding - root.rightPadding - icon.width - row.spacing
+                Layout.preferredHeight: {
+                    if (root.isCategory) {
+                        return root.compact ? implicitHeight : Math.round(implicitHeight * 1.5);
+                    }
+                    if (!root.compact && !descriptionLabel.visible) {
+                        return implicitHeight * 2;
+                    }
+                    return implicitHeight;
+                }
+                text: root.text
+                elide: Text.ElideRight
+                verticalAlignment: Text.AlignVCenter
+                maximumLineCount: 1
+            }
+
+            PC3.Label {
+                id: descriptionLabel
+                Layout.fillWidth: true
+                visible: text.length > 0 && text !== root.text
+                enabled: false
+                text: root.description
+                font: PlasmaCore.Theme.smallestFont
+                elide: Text.ElideRight
+                verticalAlignment: Text.AlignVCenter
+                horizontalAlignment: root.compact ? Text.AlignRight : Text.AlignLeft
+                maximumLineCount: 1
+            }
+        }
+    }
+}

applets/kickoff/package/contents/config/main.xml
@@ -49,5 +49,9 @@
             <label>Whether to display captions ("shut down", "log out", etc.) for the footer action buttons</label>
             <default>true</default>
         </entry>
+        <entry name="compactMode" type="Bool">
+            <label>Whether to use a compact display style for list items</label>
+            <default>false</default>
+        </entry>
     </group>
 </kcfg>

applets/kickoff/package/contents/ui/ApplicationsPage.qml
@@ -19,7 +19,7 @@ BasePage {
         model: plasmoid.rootItem.rootModel
         // needed otherwise app displayed at top-level will show a first character as group.
         section.property: ""
-        delegate: KickoffItemDelegate {
+        delegate: KickoffListDelegate {
             width: view.availableWidth
             isCategory: model.hasChildren
         }

applets/kickoff/package/contents/ui/ConfigGeneral.qml
@@ -1,6 +1,7 @@
 /*
     SPDX-FileCopyrightText: 2013 David Edmundson <davidedmundson@kde.org>
     SPDX-FileCopyrightText: 2021 Mikel Johnson <mikel5764@gmail.com>
+    SPDX-FileCopyrightText: 2022 Nate Graham <nate@kde.org>
 
     SPDX-License-Identifier: GPL-2.0-or-later
 */
@@ -22,6 +23,7 @@ ColumnLayout {
     property var cfg_systemFavorites: String(plasmoid.configuration.systemFavorites)
     property int cfg_primaryActions: plasmoid.configuration.primaryActions
     property alias cfg_showActionButtonCaptions: showActionButtonCaptions.checked
+    property alias cfg_compactMode: compactModeCheckbox.checked
 
     Kirigami.FormLayout {
         Button {
@@ -80,7 +82,22 @@ ColumnLayout {
 
         CheckBox {
             id: alphaSort
+            Kirigami.FormData.label: i18nc("General options", "General:")
             text: i18n("Always sort applications alphabetically")
+        }
+
+        CheckBox {
+            id: compactModeCheckbox
+            text: i18n("Use compact list item style")
+            checked: Kirigami.Settings.tabletMode ? true : plasmoid.configuration.compactMode
+            enabled: !Kirigami.Settings.tabletMode
+        }
+        Label {
+            visible: Kirigami.Settings.tabletMode
+            text: i18nc("@info:usagetip under a checkbox when Touch Mode is on", "Automatically disabled when in Touch Mode")
+            Layout.fillWidth: true
+            wrapMode: Text.Wrap
+            font: Kirigami.Theme.smallFont
         }
 
         Button {

applets/kickoff/package/contents/ui/FullRepresentation.qml
@@ -88,7 +88,7 @@ EmptyPage {
                 // Forces the function be re-run every time runnerModel.count changes.
                 // This is absolutely necessary to make the search view work reliably.
                 model: plasmoid.rootItem.runnerModel.count ? plasmoid.rootItem.runnerModel.modelForRow(0) : null
-                delegate: KickoffItemDelegate {
+                delegate: KickoffListDelegate {
                     width: view.availableWidth
                     isSearchResult: true
                 }

applets/kickoff/package/contents/ui/KickoffDropArea.qml
@@ -14,7 +14,7 @@ DropArea {
     property real scrollUpMargin: 0
     property real scrollDownMargin: 0
     enabled: plasmoid.immutability !== PlasmaCore.Types.SystemImmutable
-    onPositionChanged: if (drag.source instanceof KickoffItemDelegate) {
+    onPositionChanged: if (drag.source instanceof KickoffGridDelegate || drag.source instanceof KickoffListDelegate) {
         const source = drag.source
         const view = drag.source.view
         if (source.view === root.targetView && !view.move.running && !view.moveDisplaced.running) {

Sonic-DE/sonic-desktop-interface

commit e1a9ee5da68653b35065c340ab9a6d85d89614af

Disable Drag and Drop for touch, fix drag and drop icons
Drag and drop conflicted with drag/flick scrolling and drag and drop was
deemed less important than scrolling or opening the context menu for
touch. Maybe in the future we can have a way to at least rearrange
favorites with touch input.

Drag and drop icons seem to have been broken by commit
0868a5ae8154d10ccc7488034bc5b6e458a17968 and are now fixed.

BUG: 450448

applets/kickoff/package/contents/ui/AbstractKickoffItemDelegate.qml
@@ -53,6 +53,8 @@ T.ItemDelegate {
     property bool descriptionTruncated: false
     property bool descriptionVisible: true
 
+    property Item dragIconItem: null
+
     function openActionMenu(x = undefined, y = undefined) {
         if (!hasActionList) { return; }
         // fill actionList only when needed to prevent slowness when changing app categories rapidly.
@@ -133,6 +135,7 @@ T.ItemDelegate {
 
     MouseArea {
         id: mouseArea
+        property bool dragEnabled: false
         parent: root
         anchors.fill: parent
         anchors.margins: 1
@@ -148,7 +151,7 @@ T.ItemDelegate {
         acceptedButtons: Qt.LeftButton | Qt.RightButton
         drag {
             axis: Drag.XAndYAxis
-            target: root.dragEnabled ? dragItem : undefined
+            target: root.dragEnabled && mouseArea.dragEnabled ? dragItem : undefined
         }
         // Using this Item fixes drag and drop causing delegates
         // to reset to a 0 X position and overlapping each other.
@@ -172,11 +175,18 @@ T.ItemDelegate {
             // Select and focus on press to improve responsiveness and touch feedback
             view.currentIndex = index
             root.forceActiveFocus(Qt.MouseFocusReason)
+
+            // Only enable drag and drop with a mouse.
+            // We don't have a good way to handle it and drag scrolling with touch.
+            mouseArea.dragEnabled = mouse.source === Qt.MouseEventNotSynthesized
+
             // We normally try to open right click menus on press like Qt Widgets
             if (mouse.button === Qt.RightButton) {
                 root.openActionMenu(mouseX, mouseY)
-            } else if (mouse.button === Qt.LeftButton && root.dragEnabled && root.Drag.imageSource == "") {
-                iconItem.grabToImage(result => {
+            } else if (mouseArea.dragEnabled && mouse.button === Qt.LeftButton
+                && root.dragEnabled && root.dragIconItem && root.Drag.imageSource == ""
+            ) {
+                root.dragIconItem.grabToImage(result => {
                     root.Drag.imageSource = result.url
                 })
             }
@@ -184,16 +194,9 @@ T.ItemDelegate {
         onClicked: if (mouse.button === Qt.LeftButton) {
             root.action.trigger()
         }
+        // MouseEvents for pressAndHold use Qt.MouseEventSynthesizedByQt for mouse.source,
+        // which makes checking mouse.source for whether or not touch input is used useless.
         onPressAndHold: if (mouse.button === Qt.LeftButton) {
-            /* TODO: make press and hold to open menu exclusive to touch.
-             * I (ndavis) tried `if (lastDeviceType & ~(PointerDevice.Mouse | PointerDevice.TouchPad))`
-             * with a TapHandler. lastDeviceType was gotten from the EventPoint argument of the
-             * grabChanged() signal. ngraham said it wouldn't work because it was preventing single
-             * taps on touch. I didn't have a touch screen to test it with.
-             *
-             * TODO: find a good way to expose the context menu to touch input that doesn't conflict with
-             * flick to scroll or drag to reorder act like right click on press and hold (with touch).
-             */
             root.openActionMenu(mouseX, mouseY)
         }
     }

applets/kickoff/package/contents/ui/KickoffGridDelegate.qml
@@ -41,10 +41,13 @@ AbstractKickoffItemDelegate {
     labelTruncated: label.truncated
     descriptionVisible: false
 
+    dragIconItem: iconItem
+
     contentItem: ColumnLayout {
         spacing: root.spacing
 
         PlasmaCore.IconItem {
+            id: iconItem
             implicitWidth: root.icon.width
             implicitHeight: root.icon.height
             Layout.alignment: Qt.AlignHCenter | Qt.AlignBottom

applets/kickoff/package/contents/ui/KickoffListDelegate.qml
@@ -48,6 +48,8 @@ AbstractKickoffItemDelegate {
     descriptionTruncated: descriptionLabel.truncated
     descriptionVisible: descriptionLabel.visible
 
+    dragIconItem: icon
+
     contentItem: RowLayout {
         id: row
         spacing: KickoffSingleton.listItemMetrics.margins.left * 2