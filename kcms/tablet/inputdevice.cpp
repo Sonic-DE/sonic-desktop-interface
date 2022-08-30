6dbdf8b116e394e296daff2698a9eeacf6b62b15
Aleix Pol <aleixpol@kde.org>
2022-08-30 20:18:19 +0200

    kcms/tablet: Add support for binding pad's buttons to keyboard events
    Since many apps won't bind them, this way they can be used to trigger
    features meant for the keyboard.

--- kcms/tablet/inputdevice.cpp
+++ kcms/tablet/inputdevice.cpp
@@ -47,22 +47,29 @@ void InputDevice::Prop<T>::set(T newVal)
 template<typename T>
 bool InputDevice::Prop<T>::changed() const
 {
+    bool r = m_value.has_value() && m_value != m_configValue;
+    if (r)
+        qDebug() << "aaaaaaa" << m_prop.name() << m_value.has_value() << m_value.value() << m_configValue.value();
     return m_value.has_value() && m_value.value() != m_configValue;
 }
 
-InputDevice::InputDevice(QString dbusName)
+InputDevice::InputDevice(const QString &dbusName, QObject *parent)
+    : QObject(parent)
 {
     m_iface.reset(new OrgKdeKWinInputDeviceInterface(QStringLiteral("org.kde.KWin"),
                                                      QStringLiteral("/org/kde/KWin/InputDevice/") + dbusName,
                                                      QDBusConnection::sessionBus(),
                                                      this));
+    qDebug() << "new device!" << dbusName;
     connect(this, &InputDevice::leftHandedChanged, this, &InputDevice::needsSaveChanged);
     connect(this, &InputDevice::orientationChanged, this, &InputDevice::needsSaveChanged);
     connect(this, &InputDevice::outputNameChanged, this, &InputDevice::needsSaveChanged);
     connect(this, &InputDevice::outputAreaChanged, this, &InputDevice::needsSaveChanged);
 }
 
-InputDevice::~InputDevice() = default;
+InputDevice::~InputDevice()
+{
+}
 
 void InputDevice::save()
 {
