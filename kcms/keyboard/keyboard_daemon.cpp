61d32953fa6dd9ebdd24e2a76117d8fb84f5cdfa
Andrey Butirsky <butirsky@gmail.com>
2022-09-14 19:17:13 +0300

    fix: Keyboard layout shortcuts activate wrong language
    BUG: 444569

--- kcms/keyboard/keyboard_daemon.cpp
+++ kcms/keyboard/keyboard_daemon.cpp
@@ -198,6 +198,16 @@ bool KeyboardDaemon::setLayout(uint index)
         layouts.append(keyboardConfig->layouts.at(index));
         XkbHelper::initializeKeyboardLayouts(layouts);
         index = layouts.size() - 1;
+
+        /* Re-calculate indexes for layout switching Actions */
+        // extra layouts list overlaps with the main layouts loop initially by 1 position
+        auto extraLayouts = keyboardConfig->layouts.mid(keyboardConfig->layoutLoopCount() - 1);
+        // spare layout currently placed in the loop is removed from the extra layouts
+        // as it was already "moved" to the last loop position
+        extraLayouts.removeOne(layouts.last());
+        layouts.append(extraLayouts);
+
+        actionCollection->setLayoutShortcuts(layouts, rules);
     }
     return X11Helper::setGroup(index);
 }
