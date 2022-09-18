796bcc45ee9ed6475959b73c7dc9d5e1aa94d08a
Weng Xuetian <wengxt@gmail.com>
2022-09-18 16:30:21 -0700

    Make emojier use its own data instead of relying on ibus.

---
+++ emojier/emojicategory.cpp
@@ -0,0 +1,18 @@
+// Generated from emoji-test.txt
+#include "emojicategory.h"
+const QStringList &getCategoryNames()
+{
+    static const QStringList names = {
+        I18N_NOOP2("Emoji Category", "Smileys and Emotion"),
+        I18N_NOOP2("Emoji Category", "People and Body"),
+        I18N_NOOP2("Emoji Category", "Component"),
+        I18N_NOOP2("Emoji Category", "Animals and Nature"),
+        I18N_NOOP2("Emoji Category", "Food and Drink"),
+        I18N_NOOP2("Emoji Category", "Travel and Places"),
+        I18N_NOOP2("Emoji Category", "Activities"),
+        I18N_NOOP2("Emoji Category", "Objects"),
+        I18N_NOOP2("Emoji Category", "Symbols"),
+        I18N_NOOP2("Emoji Category", "Flags"),
+    };
+    return names;
+}
