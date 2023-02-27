e58a4528e4b533c07dfdb36ec2db44b8a6f25738
Jeremy Whiting <jeremy.whiting@collabora.com>
2023-02-27 12:57:20 -0700

    Use images for buttons instead of listing them.
    TODO: Add more images for other buttons.
    Add axes to gui and remove labels.

--- kcms/gamepad/kcm.cpp
+++ kcms/gamepad/kcm.cpp
@@ -12,6 +12,7 @@
 #include <QStandardPaths>
 
 #include "devicemodel.h"
+#include "joybutton.h"
 #include "joydevice.h"
 
 K_PLUGIN_FACTORY_WITH_JSON(KCMJoystickFactory, "kcm_gamepad.json", registerPlugin<KCMGamePad>();)
@@ -23,6 +24,7 @@ KCMGamePad::KCMGamePad(QObject *parent, const KPluginMetaData &metaData, const Q
 {
     qmlRegisterType<DeviceModel>("org.kde.plasma.gamepad.kcm", 1, 0, "DeviceModel");
     qmlRegisterType<JoyDevice>("org.kde.plasma.gamepad.kcm", 1, 0, "JoyDevice");
+    qmlRegisterType<JoyButton>("org.kde.plasma.gamepad.kcm", 1, 0, "JoyButton");
 }
 
 #include "kcm.moc"
