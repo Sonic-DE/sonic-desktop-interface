0510566775c5add270be40003414660128ca3124
Joshua Goins <josh@redstrate.com>
2023-03-01 22:15:38 -0500

    Add old-KCM-style tables to the advanced page
    These aren't styled properly yet, but do function

---
+++ kcms/gamepad/axesmodel.h
@@ -0,0 +1,34 @@
+/*
+    SPDX-FileCopyrightText: 2023 Joshua Goins <josh@redstrate.com>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <QAbstractTableModel>
+
+class JoyDevice;
+
+class AxesModel : public QAbstractTableModel
+{
+    Q_OBJECT
+    Q_PROPERTY(JoyDevice *device MEMBER m_device NOTIFY deviceChanged REQUIRED)
+
+public:
+    AxesModel(QObject *parent = nullptr);
+
+    int rowCount(const QModelIndex &) const override;
+    int columnCount(const QModelIndex &) const override;
+
+    QVariant data(const QModelIndex &index, int role) const override;
+
+    QHash<int, QByteArray> roleNames() const override;
+    QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
+
+signals:
+    void deviceChanged();
+
+private:
+    JoyDevice *m_device = nullptr;
+};
