cec555b7108e2775ea6b0887dac9b9eae86e781b
Jeremy Whiting <jeremy.whiting@collabora.com>
2023-03-06 12:46:52 -0700

    Rename JoyFoo to Gamepad.
    Also add an SDL button enumeration in GamepadButton to use in QML.
    Also rename layouts to gamepadtypes.
    Also split Triggers out from Sticks and remove is2D etc.

--- kcms/gamepad/gamepad.cpp
+++ kcms/gamepad/gamepad.cpp
@@ -4,7 +4,7 @@
     SPDX-License-Identifier: GPL-2.0-or-later
 */
 
-#include "joydevice.h"
+#include "gamepad.h"
 
 #include <QDebug>
 #include <QFile>
@@ -17,7 +17,7 @@
 #include <csignal>
 #include <fcntl.h>
 
-JoyDevice::JoyDevice(SDL_Joystick *joystick, SDL_GameController *controller, QObject *parent)
+Gamepad::Gamepad(SDL_Joystick *joystick, SDL_GameController *controller, QObject *parent)
     : QObject(parent)
     , m_model(i18n("Unknown Model"))
     , m_joystick(joystick)
@@ -27,7 +27,7 @@ JoyDevice::JoyDevice(SDL_Joystick *joystick, SDL_GameController *controller, QOb
 
     m_vendor = SDL_JoystickGetVendor(m_joystick);
 
-    qDebug() << "joydevice vendor: " << m_vendor;
+    qDebug() << "Gamepad vendor: " << m_vendor;
 
     m_numButtons = SDL_JoystickNumButtons(joystick);
     m_numAxes = SDL_JoystickNumAxes(joystick);
@@ -35,36 +35,42 @@ JoyDevice::JoyDevice(SDL_Joystick *joystick, SDL_GameController *controller, QOb
 
     for (int i = 0; i < SDL_CONTROLLER_BUTTON_MAX; i++) {
         if (SDL_GameControllerHasButton(m_gameController, (SDL_GameControllerButton)i)) {
-            m_buttons.push_back(new JoyButton(m_vendor, i, this));
+            m_buttons.push_back(new GamepadButton(m_vendor, i, this));
         }
     }
 
     QMap<int, QString> axesToCheck = {// Only check if we have the X axes, since we should always have the Y as well
                                       {SDL_CONTROLLER_AXIS_LEFTX, i18nc("Left joystick name", "Left Thumb")},
-                                      {SDL_CONTROLLER_AXIS_RIGHTX, i18nc("Right joystick name", "Right Thumb")},
-                                      {SDL_CONTROLLER_AXIS_TRIGGERLEFT, i18nc("Left trigger button name", "Left Trigger")},
-                                      {SDL_CONTROLLER_AXIS_TRIGGERRIGHT, i18nc("Right trigger button name", "Right Trigger")}};
+                                      {SDL_CONTROLLER_AXIS_RIGHTX, i18nc("Right joystick name", "Right Thumb")}};
+
+    QMap<int, QString> triggersToCheck = {{SDL_CONTROLLER_AXIS_TRIGGERLEFT, i18nc("Left trigger button name", "Left Trigger")},
+                                          {SDL_CONTROLLER_AXIS_TRIGGERRIGHT, i18nc("Right trigger button name", "Right Trigger")}};
 
     for (int i : axesToCheck.keys()) {
         if (SDL_GameControllerHasAxis(m_gameController, (SDL_GameControllerAxis)i)) {
             QString name = axesToCheck.value(i);
-            bool is2D = (i < 3); // Triggers are only 1D
-            m_axes.insert(i, new JoyAxis(name, i, is2D, this));
+            m_axes.insert(i, new GamepadStick(name, i, this));
         }
     }
 
+    for (int i : triggersToCheck.keys()) {
+        if (SDL_GameControllerHasAxis(m_gameController, (SDL_GameControllerAxis)i)) {
+            QString name = triggersToCheck.value(i);
+            m_triggers.insert(i, new GamepadTrigger(name, i, this));
+        }
+    }
     // TODO: temporary event loop
     auto timer = new QTimer(this);
-    connect(timer, &QTimer::timeout, this, &JoyDevice::poll);
+    connect(timer, &QTimer::timeout, this, &Gamepad::poll);
     timer->start(1);
 }
 
-JoyDevice::JoyDevice()
+Gamepad::Gamepad()
     : m_model(i18n("Unknown Model"))
 {
 }
 
-void JoyDevice::poll()
+void Gamepad::poll()
 {
     SDL_Event event = {};
     while (SDL_PollEvent(&event)) { }
@@ -80,21 +86,21 @@ void JoyDevice::poll()
     }
 
     for (int i : m_axes.keys()) {
-        if (i < 3) {
-            // Get grid data
-            int16_t x = SDL_GameControllerGetAxis(m_gameController, (SDL_GameControllerAxis)i);
-            int16_t y = SDL_GameControllerGetAxis(m_gameController, (SDL_GameControllerAxis)(i + 1));
-            float xFloat = (float)x / (float)32767;
-            float yFloat = (float)y / (float)32767;
-            QVector2D gridValue(xFloat, yFloat);
-            m_axes.value(i)->setGridValue(gridValue);
-        } else {
-            // Get trigger data
-            int16_t value = SDL_GameControllerGetAxis(m_gameController, (SDL_GameControllerAxis)i);
-            float floatValue = (float)value / 32767;
-            m_axes.value(i)->setValue(floatValue);
-        }
+        // Get grid data
+        int16_t x = SDL_GameControllerGetAxis(m_gameController, (SDL_GameControllerAxis)i);
+        int16_t y = SDL_GameControllerGetAxis(m_gameController, (SDL_GameControllerAxis)(i + 1));
+        float xFloat = (float)x / (float)32767;
+        float yFloat = (float)y / (float)32767;
+        QVector2D gridValue(xFloat, yFloat);
+        m_axes.value(i)->setGridValue(gridValue);
 
         Q_EMIT axisStateChanged(i);
+    }
+
+    for (int i : m_triggers.keys()) {
+        // Get trigger data
+        int16_t value = SDL_GameControllerGetAxis(m_gameController, (SDL_GameControllerAxis)i);
+        float floatValue = (float)value / 32767;
+        m_triggers.value(i)->setValue(floatValue);
     }
 }
---
+++ kcms/gamepad/gamepadstick.cpp
@@ -0,0 +1,33 @@
+/*
+    SPDX-FileCopyrightText: 2023 Jeremy Whiting <jpwhiting@kde.org>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "gamepadstick.h"
+
+#include <QDebug>
+
+GamepadStick::GamepadStick(QString name, int index, QObject *parent)
+    : QObject(parent)
+    , m_index(index)
+    , m_name(name)
+{
+}
+
+GamepadStick::GamepadStick()
+    : m_index(0)
+{
+}
+
+QVector2D GamepadStick::getGridValue()
+{
+    return m_gridValue;
+}
+
+void GamepadStick::setGridValue(QVector2D gridValue)
+{
+    qDebug() << "stick " << m_name << " value: " << gridValue;
+    m_gridValue = gridValue;
+    emit gridValueChanged();
+}
