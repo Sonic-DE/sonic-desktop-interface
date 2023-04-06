808d9d76560f82e9e5985bd8e1d46ad570464ebb
Ismael Asensio <isma.af@gmail.com>
2023-04-06 18:17:41 +0200

    kcms/activities: Clean-up and port to an actual QML module
    The Activities KCM used to be a QWidgets KCM with some embedded QML
    components: the activity list and the activity editor. There were
    also some unnecesary settings and many leftovers of unused code.
    
    UI Changes:
    - Remove the VirtualSwitching setting, which was already getting dropped
      from KActivitiesManagerD.
    - Remove the switching Shortcuts settings, as it isn't even complete,
      and can be better accessed from the Shortcuts KCM
    - Move the "confirm deletion prompt" to QML side
    - Move the "Create New" action to the header
    
    Code Changes:
    - Remove all the KConfigXT settings, since none of those were handled
      in this KCM, but in the recentfiles one.
    - Remove the custom private plugin and custom paths to interoperate
      with QML: we can use the KCM goodies for that
    - Clean-up unused files and code indirection boilerplate.
    
    The edit-activity dialog is still mostly unchanged, to be ported in a
    follow-up commit.

---
+++ kcms/activities/kcm_activities.h
@@ -0,0 +1,30 @@
+/*
+ *  SPDX-FileCopyrightText: 2015-2016 Ivan Cukic <ivan.cukic@kde.org>
+ *  SPDX-FileCopyrightText: 2023 Ismael Asensio <isma.af@gmail.com>
+ *
+ *  SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+ */
+
+#pragma once
+
+#include <KQuickConfigModule>
+
+class ActivitiesModule : public KQuickConfigModule
+{
+    Q_OBJECT
+    Q_PROPERTY(bool newActivityAuthorized READ newActivityAuthorized CONSTANT)
+
+public:
+    ActivitiesModule(QObject *parent, const KPluginMetaData &metaData, const QVariantList &args);
+    ~ActivitiesModule() override;
+
+    bool newActivityAuthorized() const;
+
+    Q_INVOKABLE void configureActivities();
+    Q_INVOKABLE void configureActivity(const QString &id);
+    Q_INVOKABLE void newActivity();
+    Q_INVOKABLE void deleteActivity(const QString &id);
+
+private:
+    bool m_newActivityAuthorized;
+};
