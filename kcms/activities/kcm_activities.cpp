89e1bd62690907e65795796e70ddcf27c69a81f8
Ismael Asensio <isma.af@gmail.com>
2023-04-06 18:17:41 +0200

    kcms/activities: Clean-up and port to an actual QML module
    The Activities KCM used to be a QWidgets KCM with some embedded QML
    components: the activity list and the activity editor. There were
    also some unnecesary settings and many leftovers of unused code.
    
    UI Changes:
    - Remove the VirtualSwitching setting, which was already getting dropped
      from KActivitiesManagerD.
    - Remove the switching Shortcuts settings, as it isn't even complete,
      and can be better accessed from the Shortcuts KCM
    - Move the "confirm deletion prompt" to QML side
    - Move the "Create New" action to the header
    
    Code Changes:
    - Remove all the KConfigXT settings, since none of those were handled
      in this KCM, but in the recentfiles one.
    - Remove the custom private plugin and custom paths to interoperate
      with QML: we can use the KCM goodies for that
    - Clean-up unused files and code indirection boilerplate.
    
    The edit-activity dialog is still mostly unchanged, to be ported in a
    follow-up commit.

--- kcms/activities/kcm_activities.cpp
+++ kcms/activities/kcm_activities.cpp
@@ -1,7 +1,74 @@
 /*
-    SPDX-FileCopyrightText: 2012-2016 Ivan Cukic <ivan.cukic@kde.org>
+ *  SPDX-FileCopyrightText: 2015-2016 Ivan Cukic <ivan.cukic@kde.org>
+ *  SPDX-FileCopyrightText: 2023 Ismael Asensio <isma.af@gmail.com>
+ *
+ *  SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+ */
 
-    SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
-*/
+#include "kcm_activities.h"
 
-#include "MainConfigurationWidget.h"
+#include <KAuthorized>
+#include <KLocalizedString>
+#include <KPluginFactory>
+
+#include <QMessageBox>
+#include <QProcess>
+
+#include "dialog.h"
+
+#include <KActivities/Controller>
+
+K_PLUGIN_CLASS_WITH_JSON(ActivitiesModule, "kcm_activities.json")
+
+ActivitiesModule::ActivitiesModule(QObject *parent, const KPluginMetaData &metaData, const QVariantList &args)
+    : KQuickConfigModule(parent, metaData, args)
+    , m_newActivityAuthorized(KAuthorized::authorize(QStringLiteral("plasma-desktop/add_activities")))
+{
+}
+
+ActivitiesModule::~ActivitiesModule()
+{
+}
+
+bool ActivitiesModule::newActivityAuthorized() const
+{
+    return m_newActivityAuthorized;
+}
+
+void ActivitiesModule::configureActivity(const QString &id)
+{
+    Dialog::showDialog(id);
+}
+
+void ActivitiesModule::newActivity()
+{
+    if (!m_newActivityAuthorized) {
+        return;
+    }
+    Dialog::showDialog();
+}
+
+void ActivitiesModule::deleteActivity(const QString &id)
+{
+    if (!m_newActivityAuthorized) {
+        return;
+    }
+    QMetaObject::invokeMethod(
+        this,
+        [id] {
+            KActivities::Info info(id);
+
+            if (QMessageBox::question(nullptr, i18nc("@title:window", "Delete Activity"), i18n("Are you sure you want to delete '%1'?", info.name()))
+                == QMessageBox::Yes) {
+                KActivities::Controller().removeActivity(id);
+            }
+        },
+        Qt::ConnectionType::QueuedConnection);
+}
+
+void ActivitiesModule::configureActivities()
+{
+    QProcess::startDetached(QStringLiteral("kcmshell5"), {QStringLiteral("activities")});
+}
+
+#include "kcm_activities.moc"
---
+++ kcms/activities/package/contents/ui/main.qml
@@ -0,0 +1,76 @@
+/*
+ *  SPDX-FileCopyrightText: 2015 Ivan Cukic <ivan.cukic@kde.org>
+ *  SPDX-FileCopyrightText: 2023 Ismael Asensio <isma.af@gmail.com>
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+import QtQuick 2.15
+import QtQuick.Controls 2.15 as QQC2
+import QtQuick.Layouts 1.0
+
+import org.kde.activities 0.1 as Activities
+import org.kde.kcm 1.6 as KCM
+import org.kde.kcmutils 1.0
+import org.kde.kirigami 2.19 as Kirigami
+
+
+KCM.ScrollViewKCM {
+
+    implicitWidth: 500
+    implicitHeight: 400
+
+    ConfigModule.buttons: ConfigModule.Help
+
+    view: ListView {
+        id: activitiesList
+
+        clip: true
+
+        model: Activities.ActivityModel {
+            id: kactivities
+        }
+
+        delegate: Kirigami.SwipeListItem {
+            hoverEnabled: true
+
+            contentItem: RowLayout {
+                id: row
+
+                Kirigami.Icon {
+                    id: icon
+                    height: Kirigami.Units.iconSizes.medium
+                    width: height
+                    source: model.icon
+                }
+
+                QQC2.Label {
+                    Layout.fillWidth: true
+                    text: model.name
+                }
+            }
+
+            actions: [
+                Kirigami.Action {
+                    icon.name: "configure"
+                    tooltip: i18nc("@info:tooltip", "Configure %1 activity", model.name)
+                    onTriggered: kcm.configureActivity(model.id);
+                },
+                Kirigami.Action {
+                    visible: kcm.newActivityAuthorized
+                    enabled:  activitiesList.count > 1
+                    icon.name: "edit-delete"
+                    tooltip: i18nc("@info:tooltip", "Delete %1 activity", model.name)
+                    onTriggered: kcm.deleteActivity(model.id);
+                }
+            ]
+        }
+    }
+
+    actions: Kirigami.Action {
+        visible: kcm.newActivityAuthorized
+        text: i18nd("kcm_activities5", "Create New…")
+        icon.name: "list-add"
+        onTriggered: kcm.newActivity();
+    }
+}
