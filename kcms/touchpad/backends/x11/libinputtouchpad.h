b79942c65b0d8fedfbb2d2dece52d35b6642b89b
Méven Car <meven@kde.org>
2023-05-28 10:38:21 +0200

    kcm touchpad: Update in-memory state for X11 after value change

--- kcms/touchpad/backends/x11/libinputtouchpad.h
+++ kcms/touchpad/backends/x11/libinputtouchpad.h
@@ -35,7 +35,7 @@ private:
     bool valueLoader(Prop<T> &prop);
 
     template<typename T>
-    QString valueWriter(const Prop<T> &prop);
+    QString valueWriter(Prop<T> &prop);
 
     KSharedConfigPtr m_config;
 
