b2c6b6206e5af30b338b88fe783a2a9a0e289b42
Nicolas Fella <nicolas.fella@gmx.de>
2023-08-22 22:09:10 +0200

    Port to new KNotifications action API

--- containments/desktop/plugins/desktop/infonotification.cpp
+++ containments/desktop/plugins/desktop/infonotification.cpp
@@ -42,9 +42,11 @@ void InfoNotification::show()
         m_notification->setIconName(m_iconName);
         m_notification->setTitle(m_titleText);
         m_notification->setText(m_text);
-        m_notification->setActions(QStringList() << m_acknowledgeActionText);
 
-        connect(m_notification.data(), &KNotification::action1Activated, this, &InfoNotification::acknowledged);
+        auto acknowledgeAction = new KNotificationAction(m_acknowledgeActionText);
+        connect(acknowledgeAction, &KNotificationAction::activated, this, &InfoNotification::acknowledged);
+
+        m_notification->setActions({acknowledgeAction});
 
         m_notification->sendEvent();
     }
