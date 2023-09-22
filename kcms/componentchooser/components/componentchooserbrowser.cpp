af6407479da29e44ea00d2548483b905b8940c38
Arjen Hiemstra <ahiemstra@heimr.nl>
2023-09-22 13:10:43 +0200

    kcms/componentchooser: Stop writing legacy default browser key
    The x-scheme-handler mime types are the "modern" way of setting the
    default browser. However, we were still writing the kdeglobals key,
    presumably for backward compatibility. This potentially causes issues
    because things may read that key instead of the scheme handler.
    Moreover, it is yet another key that can get out of sync.

--- kcms/componentchooser/components/componentchooserbrowser.cpp
+++ kcms/componentchooser/components/componentchooserbrowser.cpp
@@ -26,10 +26,5 @@ QStringList ComponentChooserBrowser::mimeTypes() const
 void ComponentChooserBrowser::save()
 {
     const auto storageId = m_model->data(m_model->index(m_index, 0), ApplicationModel::StorageId).toString();
-
-    BrowserSettings browserSettings;
-    browserSettings.setBrowserApplication(storageId);
-    browserSettings.save();
-
     saveMimeTypeAssociations(storageId, browserMimetypes);
 }
