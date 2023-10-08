b58240e1ebd6a81c6e5a47ada1b16f13f63d71a8
Fushan Wen <qydwhotmail@gmail.com>
2023-10-08 17:41:48 +0800

    desktop: test edit mode toolbox

---
+++ appiumtests/data/layout.js
@@ -0,0 +1,6 @@
+// SPDX-FileCopyrightText: None
+// SPDX-License-Identifier: CC0-1.0
+
+var panel = new Panel
+panel.height = 2 * Math.floor(gridUnit * 2.5 / 2)
+panel.addWidget("org.kde.plasma.kickoff")
