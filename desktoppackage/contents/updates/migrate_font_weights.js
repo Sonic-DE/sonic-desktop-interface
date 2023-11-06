dd754ce0cae4d729954a9108748ddc3351a23bd6
Akseli Lahtinen <akselmo@akselmo.dev>
2023-11-06 18:46:12 +0200

    Fix value comparison
    Signed-off-by: Akseli Lahtinen <akselmo@akselmo.dev>

--- desktoppackage/contents/updates/migrate_font_weights.js
+++ desktoppackage/contents/updates/migrate_font_weights.js
@@ -1,45 +1,38 @@
 // Find all depicted widgets and migrate their font weights from qt5 to qt6 style
 
-function migrateFontWeight(oldWeight) {
-    // Takes old weight (Qt5 weight) and returns the Qt6 equivalent
-    // Qt5 font weights: https://doc.qt.io/qt-5/qfont.html#Weight-enum
-    // Qt6 font weights: https://doc.qt.io/qt-6/qfont.html#Weight-enum
-    switch(oldWeight) {
-        case 0:
-            return 100;
-        case 12:
-            return 200;
-        case 25:
-            return 300;
-        case 50:
-            return 400;
-        case 57:
-            return 500;
-        case 63:
-            return 600;
-        case 75:
-            return 700;
-        case 81:
-            return 800;
-        case 87:
-            return 900;
-        default:
-            return 400;
-    }
-}
-
 var containments = desktops().concat(panels());
 for (var c in containments) {
     const cont = containments[c];
     const widgets = cont.widgets();
     for (var w in widgets) {
-        const widget = widgets[w];
+        var widget = widgets[w];
         switch(widget.type) {
             case "org.kde.plasma.digitalclock":
-                widget.currentConfigGroup = new Array('Appearance');
-                widget.writeConfig("fontWeight", migrateFontWeight(widget.readConfig("fontWeight")));
+                widget.currentConfigGroup = ['Appearance'];
+                print(widget.readConfig("fontWeight"));
+                const oldFontWeight = widget.readConfig("fontWeight");
+                const newFontWeight = migrateFontWeight(Number(oldFontWeight));
+                widget.writeConfig("fontWeight", newFontWeight);
                 break;
         }
     }
+}
+
+function migrateFontWeight(oldWeight) {
+    // Takes old weight (Qt5 weight) and returns the Qt6 equivalent
+    // Qt5 font weights: https://doc.qt.io/qt-5/qfont.html#Weight-enum
+    // Qt6 font weights: https://doc.qt.io/qt-6/qfont.html#Weight-enum
+    var newWeight = 400;
+    if (oldWeight === 0) { newWeight = 100; }
+    else if (oldWeight === 12) { newWeight = 200; }
+    else if (oldWeight === 25) { newWeight = 300; }
+    else if (oldWeight === 50) { newWeight = 400; }
+    else if (oldWeight === 57) { newWeight = 500; }
+    else if (oldWeight === 63) { newWeight = 600; }
+    else if (oldWeight === 75) { newWeight = 700; }
+    else if (oldWeight === 81) { newWeight = 800; }
+    else if (oldWeight === 87) { newWeight = 900; }
+
+    return newWeight;
 }
 
