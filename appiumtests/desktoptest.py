6d75f38ac5983ff7202568f5e5e8f2423276996b
Fushan Wen <qydwhotmail@gmail.com>
2023-11-23 23:39:34 +0800

    appiumtests/desktoptest: test Meta+N shortcut
    The shortcut has regressed twice since the Qt6 porting, so a test is
    necessary to consolidate it.
    
    CCBUG: 477185

--- appiumtests/desktoptest.py
+++ appiumtests/desktoptest.py
@@ -114,7 +114,7 @@ class DesktopTest(unittest.TestCase):
     def _open_containment_config_dialog(self) -> None:
         # Alt+D, S
         actions = ActionChains(self.driver)
-        actions.key_down(Keys.ALT).key_down("d").key_up("d").key_up(Keys.ALT).perform()
+        actions.key_down(Keys.ALT).send_keys("d").key_up(Keys.ALT).perform()
         time.sleep(0.5)
         actions.send_keys("s").perform()
         WebDriverWait(self.driver, 30).until(EC.presence_of_element_located((AppiumBy.NAME, "Wallpaper type:")))
@@ -173,7 +173,7 @@ class DesktopTest(unittest.TestCase):
         """
         # Alt+D, E
         actions = ActionChains(self.driver)
-        actions.key_down(Keys.ALT).key_down("d").key_up("d").key_up(Keys.ALT).perform()
+        actions.key_down(Keys.ALT).send_keys("d").key_up(Keys.ALT).perform()
         actions.send_keys("e").perform()
 
         wait = WebDriverWait(self.driver, 30)
@@ -181,10 +181,23 @@ class DesktopTest(unittest.TestCase):
         widget_button: WebElement = wait.until(EC.presence_of_element_located((AppiumBy.NAME, "Add Widgets…")))
         wait.until(EC.presence_of_element_located((AppiumBy.NAME, "Add Spacer")))
 
-        ActionChains(self.driver).send_keys(Keys.ESCAPE).perform()
+        actions.send_keys(Keys.ESCAPE).perform()
         wait.until_not(lambda _: widget_button.is_displayed())
 
         self._exit_edit_mode()
+
+    def test_4_bug477185_meta_number_shortcut(self) -> None:
+        """
+        Meta+1 should activate the first launcher item
+        """
+        wait = WebDriverWait(self.driver, 30)
+        wait.until(EC.presence_of_element_located((AppiumBy.NAME, "System Settings")))
+        message: Gio.DBusMessage = Gio.DBusMessage.new_method_call("org.kde.kglobalaccel", "/component/plasmashell", "org.kde.kglobalaccel.Component", "invokeShortcut")
+        message.set_body(GLib.Variant("(s)", ["activate task manager entry 1"]))
+        Gio.bus_get_sync(Gio.BusType.SESSION).send_message_with_reply_sync(message, Gio.DBusSendMessageFlags.NONE, 1000)
+        title_element: WebElement = wait.until(EC.presence_of_element_located((AppiumBy.NAME, "Quick Settings")))
+        ActionChains(self.driver).key_down(Keys.ALT).send_keys(Keys.F4).key_up(Keys.ALT).perform()
+        wait.until_not(lambda _: title_element.is_displayed())
 
 
 if __name__ == '__main__':
