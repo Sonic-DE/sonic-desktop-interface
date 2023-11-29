a9406af31863b31cd6998f470831106bed0e8706
Fushan Wen <qydwhotmail@gmail.com>
2023-11-30 00:48:46 +0800

    Desktop: update accent color in slot instead of binding
    Don't use binding as usedInAccentColor may be disabled immediately after
    a query from kcm_colors.

--- appiumtests/desktoptest.py
+++ appiumtests/desktoptest.py
@@ -211,7 +211,7 @@ class DesktopTest(unittest.TestCase):
 
         self._exit_edit_mode()
 
-    def test_4_bug477185_meta_number_shortcut(self) -> None:
+    def test_5_bug477185_meta_number_shortcut(self) -> None:
         """
         Meta+1 should activate the first launcher item
         """
@@ -246,6 +246,29 @@ class DesktopTest(unittest.TestCase):
         message.set_body(GLib.Variant("(s)", ["activate task manager entry 1"]))
         session_bus.send_message_with_reply_sync(message, Gio.DBusSendMessageFlags.NONE, 1000)
         wait.until(EC.presence_of_element_located((AppiumBy.NAME, "Install Software")))
+
+    def test_4_bug_query_accent_color_binding_loop(self) -> None:
+        """
+        Don't use binding as usedInAccentColor may be disabled immediately after a query from kcm_colors.
+
+        The call order is:
+        1. desktop.usedInAccentColor: true -> wallpaperColors.active: true
+        2. Kirigami.ImageColors.update()
+        3. Kirigami.ImageColors emits paletteChanged()
+        5. If binding is used, ShellCorona::colorChanged is emitted immediately in the same context after desktop.accentColor is updated
+           -> desktop.usedInAccentColor: false -> desktop.accentColor: "transparent" (binding restoration).
+        6. The second time querying the accent color, the QML engine will report binding loop detected in desktop.usedInAccentColor,
+           and desktop.accentColor will return "transparent" directly before any accent color from the wallpaper is extracted.
+        """
+        session_bus = Gio.bus_get_sync(Gio.BusType.SESSION)
+        # Send the same request twice to check binding loop
+        for _ in range(2):
+            print("sending message", file=sys.stderr)
+            message: Gio.DBusMessage = Gio.DBusMessage.new_method_call("org.kde.plasmashell", "/PlasmaShell", "org.kde.PlasmaShell", "color")
+            reply, _ = session_bus.send_message_with_reply_sync(message, Gio.DBusSendMessageFlags.NONE, 1000)
+            self.assertEqual(reply.get_signature(), "u")
+            self.assertGreater(reply.get_body().get_child_value(0).get_uint32(), 0)
+            print("done sending message", file=sys.stderr)
 
 
 if __name__ == '__main__':
--- desktoppackage/contents/views/Desktop.qml
+++ desktoppackage/contents/views/Desktop.qml
@@ -77,20 +77,15 @@ Item {
 
             onBackgroundColorChanged: Qt.callLater(update)
             onTextColorChanged: Qt.callLater(update)
-
-            property Binding colorBinding: Binding {
-                target: desktop
-                property: "accentColor"
-                value: {
+            // Don't use binding as usedInAccentColor may be disabled immediately after a query from kcm_colors
+            onPaletteChanged: {
                     if (!Qt.colorEqual(imageColors.colorFromPlugin, "transparent")) {
-                        return imageColors.colorFromPlugin;
-                    }
-                    if (imageColors.palette.length === 0) {
-                        return "transparent";
-                    }
-                    return imageColors.dominant;
+                    desktop.accentColor = imageColors.colorFromPlugin;
+                } else if (imageColors.palette.length === 0) {
+                    desktop.accentColor = "transparent";
+                } else {
+                    desktop.accentColor = imageColors.dominant;
                 }
-                when: desktop.usedInAccentColor // Without this, accentColor may still be updated after usedInAccentColor becomes false
             }
 
             property Connections repaintConnection: Connections {
@@ -104,8 +99,6 @@ Item {
                 }
             }
         }
-
-        onLoaded: item.update()
     }
 
     Timer {
