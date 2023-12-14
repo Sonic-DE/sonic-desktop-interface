KDE/plasma-desktop

commit 6ea58c0f0ac6b9d91b255a41e51032a597238476

kcms/baloo: make "current indexing" label a full path again
Only showing the filename is actually not super useful for
troubleshooting purposes, which is why this was added.

kcms/baloo/ui/main.qml
@@ -153,16 +153,27 @@ KCM.ScrollViewKCM {
                     text: i18n("%1, %2\% complete", monitor.stateString, monitor.completionPercentage)
                     elide: Text.ElideLeft
                 }
+            }
 
-                // File being indexed, if indexing
-                Kirigami.SelectableLabel {
-                    readonly property string fileName: monitor.filePath.split('\\').pop().split('/').pop()
+            Item {
+                visible: fileBeingIndexed.visible
+                implicitHeight: Kirigami.Units.smallSpacing
+            }
 
-                    Kirigami.FormData.label: i18nc("@label file currently being indexed", "Currently Indexing:")
-                    Layout.fillWidth: true
-                    visible: fileSearchEnabled.checked && monitor.currentlyIndexing && monitor.completionPercentage !== 100 && fileName.length > 0
-                    text: xi18nc("@info Currently Indexing", "<filename>%1</filename>", fileName)
-                }
+            // Current file being indexed
+            QQC2.Label {
+                Layout.fillWidth: true
+                text: i18nc("@label file currently being indexed", "Currently indexing:")
+                horizontalAlignment: Text.AlignHCenter
+                visible: fileBeingIndexed.visible
+            }
+
+            // File being indexed, if indexing
+            Kirigami.SelectableLabel {
+                id: fileBeingIndexed
+                Layout.fillWidth: true
+                visible: fileSearchEnabled.checked && monitor.completionPercentage !== 100 && monitor.filePath.length > 0
+                text: xi18nc("@info Currently Indexing", "<filename>%1</filename>", monitor.filePath)
             }
 
             Item {

Sonic-DE/sonic-desktop-interface

commit fbff22461c5358ad9778a7a30a8ec0be63e85fa1

Fix hiding of filename label when indexer is idle
This fixes a regression caused by commit 6ea58c0f0ac6 ("kcms/baloo: make
"current indexing" label a full path again"). Prior to this commit, the
condition included the boolean "monitor.currentlyIndexing" property.

Using the filePath.length and completionPercentage is insufficient,
as the filepath value is only valid when the state is *not* idle, and
may show e.g. a placeholder value during idle state.

Fixes: 477274

kcms/baloo/ui/main.qml
@@ -146,7 +146,7 @@ KCM.ScrollViewKCM {
             Kirigami.SelectableLabel {
                 id: fileBeingIndexed
                 Layout.fillWidth: true
-                visible: fileSearchEnabled.checked && monitor.completionPercentage !== 100 && monitor.filePath.length > 0
+                visible: fileSearchEnabled.checked && monitor.currentlyIndexing && monitor.completionPercentage !== 100 && monitor.filePath.length > 0
                 text: xi18nc("@info Currently Indexing", "<filename>%1</filename>", monitor.filePath)
             }
 