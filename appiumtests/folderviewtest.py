7effc0b0b3951fa613c14218a7278d276fed34a0
Fushan Wen <qydwhotmail@gmail.com>
2024-01-23 00:37:39 +0800

    appiumtests/bug477220: take screenshot using ImageMagick

--- appiumtests/folderviewtest.py
+++ appiumtests/folderviewtest.py
@@ -93,6 +93,7 @@ class FolderViewTest(unittest.TestCase):
         with tempfile.TemporaryDirectory() as temp_dir:
             # Take desktop screenshot
             saved_image_path: str = os.path.join(temp_dir, "desktop.png")
+            os.environ["DISPLAY"] = ":0"
             subprocess.check_call(["import", "-window", "root", saved_image_path])
             cv_first_image = cv.imread(saved_image_path, cv.IMREAD_COLOR)
             first_image = base64.b64encode(cv.imencode('.png', cv_first_image)[1].tobytes()).decode()
