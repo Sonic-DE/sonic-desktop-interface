KDE/plasma-desktop

commit 0d14b0b153ef2bdbe1e097edb7415f3e3cef21bc

Simplify code for applet sizes and respect max width/height of non-filling applets
Some applets don't fillWidth/Height, and yet they still set a MaxWidth/MaxHeight
(and, usually, a minWidth/minHeight that's the same value). This respects the
requested size, and tries to simplify the code using `||` as a fallback operator
instead of nested `? :` operators.

BUG: 477813
BUG: 476925

containments/panel/contents/ui/main.qml
@@ -206,24 +206,6 @@ ContainmentItem {
                 //this is completely heuristic, but looks way less "jumpy"
                 property bool movingForResize: false
 
-                readonly property bool wantsToFillWidth: applet && applet.Layout.fillWidth
-                onWantsToFillWidthChanged: {
-                    if (Plasmoid.formFactor !== PlasmaCore.Types.Vertical) {
-                        checkLastSpacer();
-                    }
-                }
-                readonly property bool wantsToFillHeight: applet && applet.Layout.fillWidth
-                onWantsToFillHeightChanged: {
-                    if (Plasmoid.formFactor === PlasmaCore.Types.Vertical) {
-                        checkLastSpacer();
-                    }
-                }
-                // Always fill width/height, in order to still shrink the applet when there is not enough space.
-                // When the applet doesn't want to expand set a Layout.maximumWidth accordingly
-                // https://bugs.kde.org/show_bug.cgi?id=473420
-                Layout.fillWidth: true
-                Layout.fillHeight: true
-
                 function getMargins(side, returnAllMargins = false, overrideFillArea = null, overrideThickArea = null): real {
                     if (!applet || !applet.plasmoid) {
                         return 0;
@@ -243,36 +225,31 @@ ContainmentItem {
                 Layout.leftMargin: getMargins('left')
                 Layout.rightMargin: getMargins('right')
 
+                // Always fill width/height, in order to still shrink the applet when there is not enough space.
+                // When the applet doesn't want to expand set a Layout.maximumWidth accordingly
+                // https://bugs.kde.org/show_bug.cgi?id=473420
+                Layout.fillWidth: true
+                Layout.fillHeight: true
+                property bool wantsToFillWidth: applet?.Layout.fillWidth
+                property bool wantsToFillHeight: applet?.Layout.fillHeight
+                property bool wantsToFillSpace: [wantsToFillHeight, wantsToFillWidth]
+                onWantsToFillSpaceChanged: checkLastSpacer()
+
+                property int availWidth: root.width - Layout.leftMargin - Layout.rightMargin
+                property int availHeight: root.height - Layout.topMargin - Layout.bottomMargin
+                function findPositive(first, second) {return first > 0 ? first : second}
+
     // BEGIN BUG 454095: do not combine these expressions to a function or the bindings won't work
-                readonly property real appletMaxWidth: applet?.Layout.maximumWidth >= 0 ? applet.Layout.maximumWidth : root.width
-                readonly property real appletPrefWidth: (applet ? (applet.Layout.preferredWidth >= 0
-                                                            ? applet.Layout.preferredWidth
-                                                            : Math.max(root.height,
-                                                                (applet.implicitWidth > 0 ?
-                                                                applet.implicitWidth : applet.Layout.minimumWidth)))
-                                                        : root.height)
-                readonly property real appletMaxHeight: applet?.Layout.maximumHeight >= 0 ? applet.Layout.maximumHeight : root.height
-                readonly property real appletPrefHeight: (applet ? (applet.Layout.preferredHeight >= 0
-                                                            ? applet.Layout.preferredHeight
-                                                            : Math.max(root.width,
-                                                                (applet.implicitHeight > 0 ?
-                                                                applet.implicitHeight : applet.Layout.minimumHeight)))
-                                                         : root.width)
+                Layout.minimumWidth: root.isHorizontal ? findPositive(applet?.Layout.minimumWidth, root.height) : availWidth
+                Layout.minimumHeight: !root.isHorizontal ? findPositive(applet?.Layout.minimumHeight, root.width) : availHeight
 
-                Layout.minimumWidth: (root.isHorizontal ? (applet && applet.Layout.minimumWidth > 0 ? applet.Layout.minimumWidth : root.height) : root.width) - Layout.leftMargin - Layout.rightMargin
-                Layout.minimumHeight: (!root.isHorizontal ? (applet && applet.Layout.minimumHeight > 0 ? applet.Layout.minimumHeight : root.width) : root.height) - Layout.bottomMargin - Layout.topMargin
+                Layout.preferredWidth: root.isHorizontal ? findPositive(applet?.Layout.preferredWidth, Layout.minimumWidth) : availWidth
+                Layout.preferredHeight: !root.isHorizontal ? findPositive(applet?.Layout.preferredHeight, Layout.minimumHeight) : availHeight
 
-                Layout.preferredWidth: (root.isHorizontal ? appletPrefWidth : root.width) - Layout.leftMargin - Layout.rightMargin
-                Layout.preferredHeight: (!root.isHorizontal ? appletPrefHeight : root.height) - Layout.bottomMargin - Layout.topMargin
-
-                // Use the preferred width as maximum in the case the applet doesn't want to fill width, see BUG:473420
-                Layout.maximumWidth: (root.isHorizontal
-                        ? (applet?.Layout.fillWidth ? appletMaxWidth : appletPrefWidth)
-                        : root.height) - Layout.leftMargin - Layout.rightMargin
-                Layout.maximumHeight: (!root.isHorizontal
-                        ? (applet?.Layout.fillHeight ? appletMaxHeight : appletPrefHeight)
-                        : root.width) - Layout.bottomMargin - Layout.topMargin
+                Layout.maximumWidth: root.isHorizontal ? (wantsToFillWidth ? findPositive(applet?.Layout.maximumWidth, root.width) : Layout.preferredWidth) : availWidth
+                Layout.maximumHeight: !root.isHorizontal ? (wantsToFillHeight ? findPositive(applet?.Layout.maximumHeight, root.height) : Layout.preferredHeight) : availHeight
     // END BUG 454095
+
                 Item {
                     id: marginHighlightElements
                     anchors.fill: parent

applets/kickoff/package/contents/ui/main.qml
@@ -204,8 +204,8 @@ PlasmoidItem {
                     return {
                         minimumWidth: -1,
                         maximumWidth: iconSize,
-                        minimumHeight: -1,
-                        maximumHeight: iconSize,
+                        minimumHeight: impHeight,
+                        maximumHeight: impHeight,
                     };
                 } else { // horizontal
                     return {

Sonic-DE/sonic-desktop-interface

commit ea964704199a28ceedf34cc79b55e81897577eff

Fix applet container's Layout.preferredWidth/Height
This fixes a regression caused by commit
0d14b0b153ef2bdbe1e097edb7415f3e3cef21bc.

This restores the default preferred size to square.

containments/panel/contents/ui/main.qml
@@ -245,8 +245,8 @@ ContainmentItem {
                 Layout.minimumWidth: root.isHorizontal ? findPositive(applet?.Layout.minimumWidth, availHeight) : availWidth
                 Layout.minimumHeight: !root.isHorizontal ? findPositive(applet?.Layout.minimumHeight, availWidth) : availHeight
 
-                Layout.preferredWidth: root.isHorizontal ? findPositive(applet?.Layout.preferredWidth, Layout.minimumWidth) : availWidth
-                Layout.preferredHeight: !root.isHorizontal ? findPositive(applet?.Layout.preferredHeight, Layout.minimumHeight) : availHeight
+                Layout.preferredWidth: root.isHorizontal ? findPositive(applet?.Layout.preferredWidth, Math.max(Layout.minimumWidth, root.height)) : availWidth
+                Layout.preferredHeight: !root.isHorizontal ? findPositive(applet?.Layout.preferredHeight, Math.max(Layout.minimumHeight, root.width)) : availHeight
 
                 Layout.maximumWidth: root.isHorizontal ? (wantsToFillWidth ? findPositive(applet?.Layout.maximumWidth, root.width) : Layout.preferredWidth) : availWidth
                 Layout.maximumHeight: !root.isHorizontal ? (wantsToFillHeight ? findPositive(applet?.Layout.maximumHeight, root.height) : Layout.preferredHeight) : availHeight