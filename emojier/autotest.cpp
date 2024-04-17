d7d51df311bceb3b5aba4e00a1f2596c99641db0
Fushan Wen <qydwhotmail@gmail.com>
2024-04-18 01:39:39 +0800

    test

---
+++ emojier/autotest.cpp
@@ -0,0 +1,36 @@
+/*
+    SPDX-FileCopyrightText: 2023 Fushan Wen <qydwhotmail@gmail.com>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#define QT_FORCE_ASSERTS 1
+
+#include <QProcess>
+#include <QSignalSpy>
+#include <QTest>
+
+using namespace Qt::StringLiterals;
+
+class TestTest : public QObject
+{
+    Q_OBJECT
+
+private Q_SLOTS:
+    void test_improveCoverage();
+};
+
+void TestTest::test_improveCoverage()
+{
+    QProcess proc;
+    proc.setProgram(u"qml6"_s);
+    proc.setArguments({QFINDTESTDATA(u"../../emojier/app/ui/Emojier.qml"_s)});
+    proc.start();
+    proc.waitForStarted();
+    proc.terminate();
+    proc.waitForFinished();
+}
+
+QTEST_GUILESS_MAIN(TestTest)
+
+#include "autotest.moc"
