6b01993fbf9280b9ba46fa11707eb33851e59c12
ivan tkachenko <me@ratijas.tk>
2024-06-03 02:29:24 +0600

    kcms/mouse: X11 device: Simplify supportsLeftHanded and leftHandedEnabledByDefault
    They are pretty much compile-time constants, they don't need to be
    backed by Prop objects.

--- kcms/mouse/backends/x11/x11_libinput_dummydevice.h
+++ kcms/mouse/backends/x11/x11_libinput_dummydevice.h
@@ -67,12 +67,12 @@ public:
     // advanced
     bool supportsLeftHanded() const override
     {
-        return m_supportsLeftHanded.val;
+        return true;
     }
 
     bool leftHandedEnabledByDefault() const override
     {
-        return m_leftHandedEnabledByDefault.val;
+        return false;
     }
 
     bool isLeftHanded() const override
@@ -261,8 +261,6 @@ private:
 
     //
     // advanced
-    Prop<bool> m_supportsLeftHanded{this};
-    Prop<bool> m_leftHandedEnabledByDefault{this};
     Prop<bool> m_leftHanded{this, &X11LibinputDummyDevice::leftHandedChanged, u"XLbInptLeftHanded"_s};
 
     Prop<bool> m_supportsMiddleEmulation{this};
