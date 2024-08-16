be1fec5e88746619c43106602ed26bc3998c6554
Akseli Lahtinen <akselmo@akselmo.dev>
2024-08-16 14:42:43 +0300

    kcm_keys: fix importing custom commands
    This adds new handling for Custom Commands,
    such as exporting them as their own group.
    
    It also waits for the adding of a component to
    be finished, because otherwise we'll face async issues.
    
    Most of the work was done by @davidre already.

--- kcms/keys/basemodel.cpp
+++ kcms/keys/basemodel.cpp
@@ -148,7 +148,8 @@ QVariant BaseModel::data(const QModelIndex &index, int role) const
         const Action &action = m_components[index.parent().row()].actions[index.row()];
         switch (role) {
         case Qt::DisplayRole: {
-            KDesktopFile desktopFile(action.id);
+            qDebug() << m_components[index.parent().row()].id;
+            KDesktopFile desktopFile(m_components[index.parent().row()].id);
             KConfigGroup cg = desktopFile.desktopGroup();
             if (cg.readEntry<bool>("X-KDE-GlobalAccel-CommandShortcut", false)) {
                 return cg.readEntry("Exec");
