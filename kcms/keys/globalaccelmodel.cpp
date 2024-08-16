3cbd171a7d090bf1a2bf97d83b965699a7950ea9
Akseli Lahtinen <akselmo@akselmo.dev>
2024-08-16 14:42:43 +0300

    kcm_keys: fix importing custom commands
    This adds new handling for Custom Commands,
    such as exporting them as their own group.
    
    It also waits for the adding of a component to
    be finished, because otherwise we'll face async issues.
    
    Most of the work was done by @davidre already.

--- kcms/keys/globalaccelmodel.cpp
+++ kcms/keys/globalaccelmodel.cpp
@@ -294,7 +294,7 @@ void GlobalAccelModel::importConfig(const KConfigBase &config)
     }
 }
 
-void GlobalAccelModel::addApplication(const QString &desktopFileName, const QString &displayName)
+void GlobalAccelModel::addApplication(const QString &desktopFileName, const QString &displayName, const bool &waitForFinish)
 {
     if (desktopFileName.isEmpty()) {
         qCWarning(KCMKEYS()) << "Tried to add empty application" << displayName;
@@ -352,7 +352,13 @@ void GlobalAccelModel::addApplication(const QString &desktopFileName, const QStr
             m_components.insert(pos, c);
             endInsertRows();
         });
+        if (waitForFinish) {
+            infoWatcher->waitForFinished();
+        }
     });
+    if (waitForFinish) {
+        watcher->waitForFinished();
+    }
 }
 
 void GlobalAccelModel::removeComponent(const Component &component)
