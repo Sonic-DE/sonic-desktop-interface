df26e84569296a3cd0bda4f4ea5e4d3ec51866b7
Akseli Lahtinen <akselmo@akselmo.dev>
2024-10-31 15:13:32 +0200

    Add screenGeometryChanged to folderModel, connect positioner to it
    Remove it from folderviewlayer

--- containments/desktop/plugins/folder/positioner.cpp
+++ containments/desktop/plugins/folder/positioner.cpp
@@ -27,6 +27,7 @@ Positioner::Positioner(QObject *parent)
     m_savePositionsTimer->setSingleShot(true);
     m_savePositionsTimer->setInterval(500);
     connect(m_savePositionsTimer, &QTimer::timeout, this, &Positioner::savePositionsConfig);
+    loadAndApplyPositionsConfig();
 }
 
 Positioner::~Positioner()
@@ -1028,6 +1029,11 @@ void Positioner::setResolution(const QString &resolution)
     }
 }
 
+void Positioner::onScreenGeometryChanged()
+{
+    loadAndApplyPositionsConfig();
+}
+
 void Positioner::connectSignals(FolderModel *model)
 {
     connect(model, &QAbstractItemModel::dataChanged, this, &Positioner::sourceDataChanged, Qt::UniqueConnection);
@@ -1042,6 +1048,7 @@ void Positioner::connectSignals(FolderModel *model)
     connect(m_folderModel, &FolderModel::urlChanged, this, &Positioner::reset, Qt::UniqueConnection);
     connect(m_folderModel, &FolderModel::statusChanged, this, &Positioner::sourceStatusChanged, Qt::UniqueConnection);
     connect(m_folderModel, &FolderModel::itemRenamed, this, &Positioner::updatePositionsList, Qt::UniqueConnection);
+    connect(m_folderModel, &FolderModel::screenGeometryChanged, this, &Positioner::onScreenGeometryChanged, Qt::UniqueConnection);
 }
 
 void Positioner::disconnectSignals(FolderModel *model)
@@ -1058,4 +1065,5 @@ void Positioner::disconnectSignals(FolderModel *model)
     disconnect(m_folderModel, &FolderModel::urlChanged, this, &Positioner::reset);
     disconnect(m_folderModel, &FolderModel::statusChanged, this, &Positioner::sourceStatusChanged);
     disconnect(m_folderModel, &FolderModel::itemRenamed, this, &Positioner::updatePositionsList);
+    disconnect(m_folderModel, &FolderModel::screenGeometryChanged, this, &Positioner::onScreenGeometryChanged);
 }
