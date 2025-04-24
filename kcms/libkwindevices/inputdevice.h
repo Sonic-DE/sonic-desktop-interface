saidbakr/plasma-desktop

[kcms/tablet] Add setting for relative mode
CCBUG: 477898

kcms/libkwindevices/inputdevice.h
@@ -29,6 +29,7 @@ class InputDevice : public QObject
     Q_PROPERTY(int orientation READ orientation WRITE setOrientation NOTIFY orientationChanged)
     Q_PROPERTY(QString outputName READ outputName WRITE setOutputName NOTIFY outputNameChanged)
     Q_PROPERTY(QRectF outputArea READ outputArea WRITE setOutputArea NOTIFY outputAreaChanged)
+    Q_PROPERTY(bool relative READ isRelative WRITE setRelative NOTIFY relativeChanged)
 
 public:
     InputDevice(const QString &dbusName, QObject *parent);
@@ -91,6 +92,13 @@ public:
 
     void setEnabled(bool enabled);
 
+    bool isRelative() const
+    {
+        return m_relative.value();
+    }
+
+    void setRelative(bool relative);
+
 Q_SIGNALS:
     void needsSaveChanged();
 
@@ -99,6 +107,7 @@ Q_SIGNALS:
     void outputNameChanged();
     void outputAreaChanged();
     void enabledChanged();
+    void relativeChanged();
 
 private:
     template<typename T>
@@ -214,6 +223,7 @@ private:
                                              &OrgKdeKWinInputDeviceInterface::defaultOutputArea,
                                              &OrgKdeKWinInputDeviceInterface::supportsOutputArea,
                                              &InputDevice::outputAreaChanged);
+    Prop<bool> m_relative = Prop<bool>(this, "relative", nullptr, nullptr, &InputDevice::relativeChanged);
 
     std::unique_ptr<OrgKdeKWinInputDeviceInterface> m_iface;
 };

Sonic-DE/sonic-desktop-interface

kcms/libkwindevices/inputdevice.h
@@ -43,6 +43,7 @@ class InputDevice : public QObject
     Q_PROPERTY(double pressureRangeMin READ pressureRangeMin WRITE setPressureRangeMin NOTIFY pressureRangeMinChanged)
     Q_PROPERTY(double pressureRangeMax READ pressureRangeMax WRITE setPressureRangeMax NOTIFY pressureRangeMaxChanged)
     Q_PROPERTY(bool supportsPressureRange READ supportsPressureRange CONSTANT)
+    Q_PROPERTY(bool relative READ isRelative WRITE setRelative NOTIFY relativeChanged)
 
 public:
     InputDevice(const QString &dbusName, QObject *parent);
@@ -203,6 +204,13 @@ public:
         return m_tabletTool.value();
     }
 
+    bool isRelative() const
+    {
+        return m_relative.value();
+    }
+
+    void setRelative(bool relative);
+
 Q_SIGNALS:
     void needsSaveChanged();
 
@@ -217,6 +225,7 @@ Q_SIGNALS:
     void pressureRangeMinChanged();
     void pressureRangeMaxChanged();
     void calibrationMatrixChanged();
+    void relativeChanged();
 
 private:
     template<typename T>
@@ -282,7 +291,7 @@ private:
             if (!m_value) {
                 value();
             }
-        
+
             Q_ASSERT(isSupported());
             if (m_value != newVal) {
                 m_value = newVal;
@@ -296,7 +305,7 @@ private:
         {
             return m_defaultValueFunction ? (m_device->m_iface.get()->*m_defaultValueFunction)() : T();
         }
-        
+
         bool changed() const {
             return m_value.has_value() && m_value.value() != m_configValue;
         }
@@ -317,7 +326,7 @@ private:
                 qCDebug(LIBKWINDEVICES) << "skipping" << this << m_value.has_value() << isSupported() << m_prop.name();
                 return false;
             }
-        
+
             auto iface = m_device->m_iface.get();
             const bool ret = m_prop.write(iface, *m_value);
             if (ret) {
@@ -370,6 +379,7 @@ private:
                                             &OrgKdeKWinInputDeviceInterface::defaultInputArea,
                                             &OrgKdeKWinInputDeviceInterface::supportsInputArea,
                                             &InputDevice::inputAreaChanged);
+    Prop<bool> m_relative = Prop<bool>(this, "tabletToolIsRelative", nullptr, nullptr, &InputDevice::relativeChanged);
 
     Prop<bool> m_mapToWorkspace =
         Prop<bool>(this, "mapToWorkspace", &OrgKdeKWinInputDeviceInterface::defaultMapToWorkspace, nullptr, &InputDevice::mapToWorkspaceChanged);