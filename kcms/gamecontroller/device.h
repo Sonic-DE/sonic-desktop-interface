KDE/plasma-desktop

Also show trigger values in AxesModel if present.
Parse Left and Right trigger events from SDL Joystick API and expose
as additional axes model values if present.

kcms/gamecontroller/device.h
@@ -19,6 +19,8 @@ class Device : public QObject
 
     Q_PROPERTY(QVector2D leftAxis READ leftAxisValue NOTIFY leftAxisChanged)
     Q_PROPERTY(QVector2D rightAxis READ rightAxisValue NOTIFY rightAxisChanged)
+    Q_PROPERTY(float leftTrigger READ leftTriggerValue NOTIFY leftTriggerChanged)
+    Q_PROPERTY(float rigthTrigger READ rightTriggerValue NOTIFY rightTriggerChanged)
 
 public:
     Device(int deviceIndex, QObject *parent = nullptr);
@@ -39,6 +41,8 @@ public:
     int axisCount() const;
     QVector2D leftAxisValue() const;
     QVector2D rightAxisValue() const;
+    float leftTriggerValue() const;
+    float rightTriggerValue() const;
 
     int hatCount() const;
     QVector2D hatPosition(int index) const;
@@ -47,6 +51,8 @@ Q_SIGNALS:
     void buttonStateChanged(int index);
     void leftAxisChanged();
     void rightAxisChanged();
+    void leftTriggerChanged();
+    void rightTriggerChanged();
     void hatPositionChanged(int index);
 
 private:
@@ -59,5 +65,7 @@ private:
     int m_deviceIndex = -1;
     QVector2D m_leftAxis;
     QVector2D m_rightAxis;
+    float m_leftTrigger;
+    float m_rightTrigger;
     SDL_Joystick *m_joystick = nullptr;
 };

Sonic-DE/sonic-desktop-interface

kcms/gamecontroller/device.h
@@ -19,6 +19,8 @@ class Device : public QObject
 
     Q_PROPERTY(QVector2D leftAxis READ leftAxisValue NOTIFY leftAxisChanged)
     Q_PROPERTY(QVector2D rightAxis READ rightAxisValue NOTIFY rightAxisChanged)
+    Q_PROPERTY(float leftTrigger READ leftTriggerValue NOTIFY leftTriggerChanged)
+    Q_PROPERTY(float rightTrigger READ rightTriggerValue NOTIFY rightTriggerChanged)
 
 public:
     Device(int deviceIndex, QObject *parent = nullptr);
@@ -39,6 +41,8 @@ public:
     int axisCount() const;
     QVector2D leftAxisValue() const;
     QVector2D rightAxisValue() const;
+    float leftTriggerValue() const;
+    float rightTriggerValue() const;
 
     int hatCount() const;
     QVector2D hatPosition(int index) const;
@@ -47,6 +51,8 @@ Q_SIGNALS:
     void buttonStateChanged(int index);
     void leftAxisChanged();
     void rightAxisChanged();
+    void leftTriggerChanged();
+    void rightTriggerChanged();
     void hatPositionChanged(int index);
 
 private:
@@ -59,5 +65,7 @@ private:
     int m_deviceIndex = -1;
     QVector2D m_leftAxis;
     QVector2D m_rightAxis;
+    float m_leftTrigger;
+    float m_rightTrigger;
     SDL_Joystick *m_joystick = nullptr;
 };