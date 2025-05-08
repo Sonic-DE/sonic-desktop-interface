4fb86d72b958510e8d1a13ff2eadb9d10c70467d
Akseli Lahtinen <akselmo@akselmo.dev>
2025-05-08 16:48:06 +0300

    FolderModel: When adding files, keep the current sorting
    The sort mode got changed to unsorted if any file was added.
    This was probably due to the infinite while loop that would happen
    later when we're looking for a new blank spot from an index
    that does not exist: This would return an empty QVariant that is
    false by default.
    
    We need to check if the index is valid. If it's not, we know
    it's a new index and can create one, so it is a blank spot.
    
    BUG: 482470

--- containments/desktop/plugins/folder/foldermodel.cpp
+++ containments/desktop/plugins/folder/foldermodel.cpp
@@ -199,13 +199,6 @@ FolderModel::FolderModel(QObject *parent)
     m_dirModel->setDropsAllowed(KDirModel::DropOnDirectory | KDirModel::DropOnLocalExecutable);
     m_dirModel->dirLister()->setAutoUpdate(true);
 
-    // If we have dropped items queued for moving, go unsorted now.
-    connect(this, &QAbstractItemModel::rowsAboutToBeInserted, this, [this]() {
-        if (!m_dropTargetPositions.isEmpty()) {
-            setSortMode(-1);
-        }
-    });
-
     /*
      * Dropped files may not actually show up as new files, e.g. when we overwrite
      * an existing file. Or files that fail to be listed by the dirLister, or...
@@ -1406,6 +1399,12 @@ void FolderModel::changeSelection(const QItemSelection &selected, const QItemSel
 bool FolderModel::isBlank(int row) const
 {
     if (row < 0) {
+        return true;
+    }
+
+    // If index is not valid, return true since that spot is blank.
+    const auto idx = index(row, 0);
+    if (!idx.isValid()) {
         return true;
     }
 
