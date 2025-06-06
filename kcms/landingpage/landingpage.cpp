f4c68230c68e42e1e1ff0eed6d47cca5843e5f76
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2025-06-06 15:20:32 +0300

    kcms/landingpage: Add auto theme switch

--- kcms/landingpage/landingpage.cpp
+++ kcms/landingpage/landingpage.cpp
@@ -7,6 +7,7 @@
 */
 
 #include "landingpage.h"
+#include "splititem.h"
 
 #include <KLocalizedString>
 #include <KPackage/PackageLoader>
@@ -179,6 +180,7 @@ KCMLandingPage::KCMLandingPage(QObject *parent, const KPluginMetaData &metaData)
     qmlRegisterAnonymousType<LandingPageGlobalsSettings>("org.kde.plasma.landingpage.kcm", 0);
     qmlRegisterAnonymousType<MostUsedModel>("org.kde.plasma.landingpage.kcm", 0);
     qmlRegisterAnonymousType<LookAndFeelGroup>("org.kde.plasma.landingpage.kcm", 0);
+    qmlRegisterType<SplitItem>("org.kde.plasma.landingpage.kcm", 1, 0, "SplitView");
 
     setButtons(Apply);
 
@@ -237,6 +239,11 @@ LookAndFeelGroup *KCMLandingPage::defaultLightLookAndFeel() const
 LookAndFeelGroup *KCMLandingPage::defaultDarkLookAndFeel() const
 {
     return m_defaultDarkLookAndFeel;
+}
+
+QString KCMLandingPage::defaultLookAndFeelPackage() const
+{
+    return m_data->settings()->defaultLookAndFeelPackageValue();
 }
 
 Q_INVOKABLE void KCMLandingPage::openKCM(const QString &kcm)
