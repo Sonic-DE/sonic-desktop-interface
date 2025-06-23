KDE/plasma-desktop

Only interact with file and folders when directly touching them
BUG:505038

containments/desktop/package/contents/ui/FolderView.qml
@@ -101,7 +101,7 @@ FocusScope {
             }
 
             var pos = mapToItem(gridView.contentItem, x, y);
-            var item = gridView.itemAt(pos.x, pos.y);
+            var item = gridView.safeItemAt(pos.x, pos.y);
 
             if (item && item.isDir) {
                 hoveredItem = item;
@@ -120,7 +120,7 @@ FocusScope {
     }
 
     function dropItemAt(pos) {
-        var item = gridView.itemAt(pos.x, pos.y);
+        var item = gridView.safeItemAt(pos.x, pos.y);
 
         if (item) {
             if (item.blank) {
@@ -142,9 +142,9 @@ FocusScope {
 
     function drop(target, event, pos) {
         var dropPos = mapToItem(gridView.contentItem, pos.x, pos.y);
-        var dropIndex = gridView.indexAt(dropPos.x, dropPos.y);
+        var dropIndex = gridView.safeIndexAt(dropPos.x, dropPos.y);
         var dragPos = mapToItem(gridView.contentItem, listener.dragX, listener.dragY);
-        var dragIndex = gridView.indexAt(dragPos.x, dragPos.y);
+        var dragIndex = gridView.safeIndexAt(dragPos.x, dragPos.y);
 
         if (listener.dragX === -1 || dragIndex !== dropIndex) {
             dir.drop(target, event, dropItemAt(dropPos), root.isContainment && !Plasmoid.immutable);
@@ -269,6 +269,7 @@ FocusScope {
         }
 
         onPressed: mouse => {
+
             // Ignore press events outside the viewport (i.e. on scrollbars).
             if (!scrollArea.viewport.contains(Qt.point(mouse.x, mouse.y))) {
                 return;
@@ -295,7 +296,7 @@ FocusScope {
                 editor.commit();
             }
 
-            var index = gridView.indexAt(mouse.x, mouse.y);
+            var index = gridView.safeIndexAt(mouse.x, mouse.y);
             var indexItem = gridView.itemAtIndex(index);
 
             if (indexItem && indexItem.iconArea) { // update position in case of touch or untriggered hover
@@ -475,7 +476,7 @@ FocusScope {
             gridView.shiftPressed = (mouse.modifiers & Qt.ShiftModifier);
 
             const mappedPos = mapToItem(gridView.contentItem, mouse.x, mouse.y)
-            var item = gridView.itemAt(mappedPos.x, mappedPos.y);
+            var item = gridView.safeItemAt(mappedPos.x, mappedPos.y);
             var leftEdge = Math.min(gridView.contentX, gridView.originX);
 
             if (!item || item.blank) {
@@ -716,6 +717,28 @@ FocusScope {
                 boundsBehavior: Flickable.StopAtBounds
                 focus: true
 
+                // itemAt returns the item that's in the cell at
+                // coordinates x, y, even if it's smaller than it.
+                // safeItemAt checks if x, y is actually within the
+                // element within the cell.
+                function safeItemAt(x, y) {
+                    let item = itemAt(x, y)
+                    if (!item) return;
+                    let coord = item.mapFromItem(gridView, x, y)
+                    if (!item.contains(coord)) {
+                        console.log("nopety nope", item, coord)
+                        return
+                    }
+                    return item;
+                }
+
+                function safeIndexAt(x, y) {
+                    if (safeItemAt(x, y)) {
+                        return indexAt(x, y)
+                    }
+                    return -1
+                }
+
                 PlasmaComponents.ScrollBar.vertical: PlasmaComponents.ScrollBar {
                     id: verticalScrollBar
                 }
@@ -766,7 +789,7 @@ FocusScope {
 
                 delegate: FolderItemDelegate {
                     width: gridView.cellWidth
-                    height: gridView.cellHeight
+                    height: contentHeight || gridView.cellHeight
                 }
 
                 onContentXChanged: {

Sonic-DE/sonic-desktop-interface

containments/desktop/package/contents/ui/FolderView.qml
@@ -101,7 +101,7 @@ FocusScope {
             }
 
             var pos = mapToItem(gridView.contentItem, x, y);
-            var item = gridView.itemAt(pos.x, pos.y);
+            var item = gridView.safeItemAt(pos.x, pos.y);
 
             if (item && item.isDir) {
                 hoveredItem = item;
@@ -120,7 +120,7 @@ FocusScope {
     }
 
     function dropItemAt(pos) {
-        var item = gridView.itemAt(pos.x, pos.y);
+        var item = gridView.safeItemAt(pos.x, pos.y);
 
         if (item) {
             if (item.blank) {
@@ -142,9 +142,9 @@ FocusScope {
 
     function drop(target, event, pos) {
         var dropPos = mapToItem(gridView.contentItem, pos.x, pos.y);
-        var dropIndex = gridView.indexAt(dropPos.x, dropPos.y);
+        var dropIndex = gridView.safeIndexAt(dropPos.x, dropPos.y);
         var dragPos = mapToItem(gridView.contentItem, listener.dragX, listener.dragY);
-        var dragIndex = gridView.indexAt(dragPos.x, dragPos.y);
+        var dragIndex = gridView.safeIndexAt(dragPos.x, dragPos.y);
 
         if (listener.dragX === -1 || dragIndex !== dropIndex) {
             dir.drop(target, event, dropItemAt(dropPos), root.isContainment && !Plasmoid.immutable);
@@ -269,6 +269,7 @@ FocusScope {
         }
 
         onPressed: mouse => {
+
             // Ignore press events outside the viewport (i.e. on scrollbars).
             if (!scrollArea.viewport.contains(Qt.point(mouse.x, mouse.y))) {
                 return;
@@ -296,7 +297,7 @@ FocusScope {
             }
 
             const mappedPos = mapToItem(gridView.contentItem, mouse.x, mouse.y)
-            var index = gridView.indexAt(mappedPos.x, mappedPos.y);
+            var index = gridView.safeIndexAt(mappedPos.x, mappedPos.y);
             var indexItem = gridView.itemAtIndex(index);
 
             if (indexItem && indexItem.iconArea) { // update position in case of touch or untriggered hover
@@ -476,7 +477,7 @@ FocusScope {
             gridView.shiftPressed = (mouse.modifiers & Qt.ShiftModifier);
 
             const mappedPos = mapToItem(gridView.contentItem, mouse.x, mouse.y)
-            var item = gridView.itemAt(mappedPos.x, mappedPos.y);
+            var item = gridView.safeItemAt(mappedPos.x, mappedPos.y);
             var leftEdge = Math.min(gridView.contentX, gridView.originX);
 
             if (!item || item.blank) {
@@ -717,6 +718,28 @@ FocusScope {
                 boundsBehavior: Flickable.StopAtBounds
                 focus: true
 
+                // itemAt returns the item that's in the cell at
+                // coordinates x, y, even if it's smaller than it.
+                // safeItemAt checks if x, y is actually within the
+                // element within the cell.
+                function safeItemAt(x, y) {
+                    let item = itemAt(x, y)
+                    if (!item) return;
+                    let coord = item.mapFromItem(gridView, x, y)
+                    if (!item.contains(coord)) {
+                        console.log("nopety nope", item, coord)
+                        return
+                    }
+                    return item;
+                }
+
+                function safeIndexAt(x, y) {
+                    if (safeItemAt(x, y)) {
+                        return indexAt(x, y)
+                    }
+                    return -1
+                }
+
                 PlasmaComponents.ScrollBar.vertical: PlasmaComponents.ScrollBar {
                     id: verticalScrollBar
                 }
@@ -767,7 +790,7 @@ FocusScope {
 
                 delegate: FolderItemDelegate {
                     width: gridView.cellWidth
-                    height: gridView.cellHeight
+                    height: contentHeight || gridView.cellHeight
                 }
 
                 onContentXChanged: {