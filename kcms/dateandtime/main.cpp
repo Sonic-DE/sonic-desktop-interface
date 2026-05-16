2ce88cabe148698fabf795ce925af5da54325ba1
Heiko Becker <heiko.becker@kde.org>
2026-05-16 19:02:44 +0200

    Drop unnecessary KF6Auth dependency
    (cherry picked from commit f85cb441f7c2ffd3e72a8a5fe141cd8a9d214c8a)

--- kcms/dateandtime/main.cpp
+++ kcms/dateandtime/main.cpp
@@ -20,9 +20,6 @@
 #include "dtime.h"
 #include "helper.h"
 
-#include <KAuth/Action>
-#include <KAuth/ExecuteJob>
-
 #include "timedated_interface.h"
 
 K_PLUGIN_CLASS_WITH_JSON(KclockModule, "kcm_clock.json")
